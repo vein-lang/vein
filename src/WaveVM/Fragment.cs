946015c55da74ccbaaa88f39cf57629c752744c9
Yuuki Wesp <ivysola@0xf6.moe>
2020-02-10 21:59:01 +0300

    add main fragment for impls instructions

---
+++ src/WaveVM/Fragment.cs
@@ -0,0 +1,13 @@
+﻿namespace wave
+{
+    public abstract class Fragment
+    {
+        private readonly byte _code;
+
+        protected Fragment(byte code) => _code = code;
+
+        public static implicit operator byte(Fragment f) => f._code;
+
+        protected abstract string ToTemplateString();
+    }
+}
