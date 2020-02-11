f28659cd2a50d531e57d9a14a6ab57f86a74516b
Yuuki Wesp <ls-micro@ya.ru>
2020-02-11 03:20:04 +0300

    add internal type handler attribute

---
+++ src/WaveVM/emit/opcodes/__ophandler.cs
@@ -0,0 +1,13 @@
+﻿namespace wave.emit.opcodes
+{
+    using System;
+    [AttributeUsage(AttributeTargets.Field)]
+    internal class __ophandler : Attribute
+    {
+        private readonly Type _typeOfOpCode;
+
+        public __ophandler(Type typeOfOpCode) => _typeOfOpCode = typeOfOpCode;
+
+        public Type GetOpCodeType() => _typeOfOpCode;
+    }
+}
