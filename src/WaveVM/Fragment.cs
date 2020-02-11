9526565fa02f641b1de740e06d3ae05a5da9bcdf
Yuuki Wesp <ls-micro@ya.ru>
2020-02-11 03:22:32 +0300

    add FragmentWithRegisterSlot

--- src/WaveVM/Fragment.cs
+++ src/WaveVM/Fragment.cs
@@ -1,13 +1,37 @@
 ﻿namespace wave
 {
+    using emit.opcodes;
+    using runtime.emit.@unsafe;
+
     public abstract class Fragment
     {
-        private readonly byte _code;
+        private readonly OpCodeValues _code;
 
-        protected Fragment(byte code) => _code = code;
+        protected Fragment(OpCodeValues code) => _code = code;
 
-        public static implicit operator byte(Fragment f) => f._code;
+        public static implicit operator byte(Fragment f) => (byte)f._code;
 
         protected abstract string ToTemplateString();
+    }
+
+    public abstract class FragmentWithRegisterSlot : Fragment, IArg<byte>
+    {
+        protected readonly byte _register;
+        protected readonly byte _slot;
+
+        protected FragmentWithRegisterSlot(string register, string slot, OpCodeValues code) : base(code)
+        {
+            _register = Storage.GetRegisterByLabel(register);
+            _slot = Storage.GetSlotByLabel(slot);
+        }
+
+        protected FragmentWithRegisterSlot(byte register, byte slot, OpCodeValues code) : base(code)
+        {
+            _register = register;
+            _slot = slot;
+        }
+
+        public byte Get()
+            => d8u.Null.Construct(_register, _slot);
     }
 }
