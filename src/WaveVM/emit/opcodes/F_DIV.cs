b5d3f4fce26c46a7e5398cdf1652fafd3ffc1659
Yuuki Wesp <ls-micro@ya.ru>
2020-02-11 03:20:12 +0300

    add idiv impl

---
+++ src/WaveVM/emit/opcodes/F_DIV.cs
@@ -0,0 +1,11 @@
+﻿namespace wave.emit.opcodes
+{
+    public class F_IDIV : FragmentWithRegisterSlot
+    {
+        protected override string ToTemplateString()
+            => $":idiv {Storage.GetRegisterByIndex(_register)}, {Storage.GetSlotByIndex(_slot)}";
+
+        public F_IDIV(string register, string slot) : base(register, slot, OpCodeValues.idiv) { }
+        public F_IDIV(byte register, byte slot) : base(register, slot, OpCodeValues.idiv) { }
+    }
+}
