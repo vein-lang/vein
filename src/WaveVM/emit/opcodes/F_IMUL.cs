dd97aac793680e9f4b25cb478bc8415a8caa00b6
Yuuki Wesp <ls-micro@ya.ru>
2020-02-11 03:20:38 +0300

    change arg of code in imul instruction

--- src/WaveVM/emit/opcodes/F_IMUL.cs
+++ src/WaveVM/emit/opcodes/F_IMUL.cs
@@ -1,16 +1,12 @@
 ﻿namespace wave.emit.opcodes
 {
-    public class F_IMUL : F_MV
+    public class F_IMUL : FragmentWithRegisterSlot
     {
-        public F_IMUL(string register, string slot) : base(register, slot)
-        {
-        }
-
-        public F_IMUL(byte register, byte slot) : base(register, slot)
-        {
-        }
 
         protected override string ToTemplateString()
             => $":imul {Storage.GetRegisterByIndex(_register)}, {Storage.GetSlotByIndex(_slot)}";
+
+        public F_IMUL(string register, string slot) : base(register, slot, OpCodeValues.imul)  { }
+        public F_IMUL(byte register, byte slot) : base(register, slot , OpCodeValues.imul) { }
     }
 }
