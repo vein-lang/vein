db3b677c95583ff765e4dc53720b392b2253d052
Yuuki Wesp <ls-micro@ya.ru>
2020-12-23 21:09:40 +0300

    update script gen.csx

--- metadata/gen.csx
+++ metadata/gen.csx
@@ -1,19 +1,19 @@
+#!/bin/dotnet run
 #r "nuget: Newtonsoft.Json, 12.0.3"
 using System;
 using System.IO;
+using System.Linq;
 using Newtonsoft.Json;
-class OpCode {
-    public string v;
-    public string n;
-}
+
+record OpCode(string n);
 
 var content = File.ReadAllText("opcodes.def.json");
 var ops = JsonConvert.DeserializeObject<OpCode[]>(content);
 
 var result = new StringBuilder();
 
-foreach(var i in ops)
-    result.AppendLine($"OP_DEF({i.n.Replace(".", "_")}, {i.v})");
+foreach(var i in ops.Select((x, y) => (x.n, y)))
+    result.AppendLine($"OP_DEF({i.n.Replace(".", "_")}, 0x{i.y:X2})");
 
 if (File.Exists("opcodes.def"))
     File.Delete("opcodes.def");
--- src/entry.cpp
+++ src/entry.cpp
@@ -1,3 +1,4 @@
+// ReSharper disable CppDeprecatedRegisterStorageClassSpecifier
 #include "core.h"
 #include "stack.h"
 #include "interp.h"
@@ -39,8 +40,8 @@ void exec_method(MetaMethodHeader* mh, stackval* args)
 {
     w_print("@exec::");
     auto* const stack = cast_t<stackval*>(calloc(mh->max_stack, sizeof(stackval)));
-    register stackval* sp = stack;
-    register unsigned char* ip = mh->code;
+    register auto sp = stack;
+    register auto ip = mh->code;
     register unsigned int level = 0;
 
     auto* end = ip + mh->code_size;
@@ -48,7 +49,7 @@ void exec_method(MetaMethodHeader* mh, stackval* args)
     stackval locals[16];
     while (1)
     {
-        #if DEBUG && !ARDUINO
+        #if defined(DEBUG) && !defined(ARDUINO)
         {
             for (auto h = 0; h < level; ++h)
                 d_print("\t");
@@ -63,51 +64,51 @@ void exec_method(MetaMethodHeader* mh, stackval* args)
         {
             CASE(NOP)
                 ASM("nop");
-            ++ip;
-            break;
+                ++ip;
+                break;
             CASE(ADD)
                 A_OPERATION(+= );
-            break;
+                break;
             CASE(SUB)
                 A_OPERATION(-= );
-            break;
+                break;
             CASE(MUL)
                 A_OPERATION(*= );
-            break;
+                break;
             CASE(DIV)
                 A_OPERATION(/= );
-            break;
+                break;
             CASE(LDARG_0)
-                CASE(LDARG_1)
-                CASE(LDARG_2)
-                CASE(LDARG_3)
-                CASE(LDARG_4)
-                * sp = args[(*ip) - LDARG_0];
-            ++sp;
-            ++ip;
-            break;
-
+            CASE(LDARG_1)
+            CASE(LDARG_2)
+            CASE(LDARG_3)
+            CASE(LDARG_4)
+                *sp = args[(*ip) - LDARG_0];
+                ++sp;
+                ++ip;
+                break;
             CASE(LDC_I32_0)
                 ++ip;
-            sp->type = VAL_I32;
-            sp->data.i = -1;
-            ++sp;
-            break;
+                sp->type = VAL_I32;
+                sp->data.i = -1;
+                ++sp;
+                break;
             CASE(DUMP_0)
                 ++ip;
-            f_print(sp[-1].type);
-            f_print(sp[-1].data.i);
+                f_print(sp[-1].type);
+                f_print(sp[-1].data.i);
+                break;
             CASE(HALT)
                 ++ip;
-            return;
+                return;
             CASE(CALL)
                 ++ip;
-            break;
-    default:
-        d_print("Unimplemented opcode: ");
-        d_print(*ip);
-        d_print("\n");
-        return;
+                break;
+            default:
+                d_print("Unimplemented opcode: ");
+                d_print(*ip);
+                d_print("\n");
+                return;
         }
     }
 
