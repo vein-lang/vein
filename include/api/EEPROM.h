e0eb8d88391d583dffd0f0e0c9e2aa7a81a887c5
Yuuki Wesp <ls-micro@ya.ru>
2020-12-29 09:50:50 +0300

    add eeprom for windows

--- include/api/EEPROM.h
+++ include/api/EEPROM.h
@@ -3,4 +3,6 @@
 
 #if defined(ARDUINO_ARCH_SAM)
 #include "SAM/eeprom_sam.h"
+#elif defined(WIN32)
+#include "Windows/eeprom_windows.h"
 #endif
---
+++ include/api/Windows/eeprom_windows.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "compatibility.types.h"
+
+class EEPROM {
+public:
+	EEPROM();
+	~EEPROM();
+	byte read(uint32_t address);
+	byte* readAddress(uint32_t address, uint32_t size);
+    bool write(uint32_t address, byte value);
+	bool write(uint32_t address, byte* data, uint32_t dataLength);
+};
