de56e48fc8d5a4dea760000257e5d834042470e4
Yuuki Wesp <ls-micro@ya.ru>
2021-01-05 07:43:26 +0300

    rework hash functions

--- include/hash.h
+++ include/hash.h
@@ -1,33 +1,45 @@
 #pragma once
 #include "compatibility.types.h"
 
-uint32_t w_hash_direct(const wpointer v1)
-{
-	return static_cast<uint32_t>(reinterpret_cast<intptr_t>(v1));
-}
+template<class T> struct hash_gen {};
 
-uint32_t w_hash_int(const wpointer v1)
-{
-	return w_hash_direct(v1);
-}
 
-uint32_t w_hash_str(const wpointer v1)
-{
-    const auto* ch_ptr1 = static_cast<const char*>(v1);
-    auto num1 = 0x1505;
-    auto num2 = num1;
-    const auto* ch_ptr2 = ch_ptr1;
-    int num3;
-    while ((num3 = *ch_ptr2) != 0)
-    {
-        num1 = (num1 << 5) + num1 ^ num3;
-        const auto num4 = static_cast<int>(ch_ptr2[1]);
-        if (num4 != 0)
+template<> struct hash_gen<char*> {
+    static size_t getHashCode(const char* v1) {
+        const auto* ch_ptr1 = static_cast<const char*>(v1);
+        auto num1 = 0x1505;
+        auto num2 = num1;
+        const auto* ch_ptr2 = ch_ptr1;
+        int num3;
+        while ((num3 = *ch_ptr2) != 0)
         {
-            num2 = (num2 << 5) + num2 ^ num4;
-            ch_ptr2 += 2;
+            num1 = (num1 << 5) + num1 ^ num3;
+            const auto num4 = static_cast<int>(ch_ptr2[1]);
+            if (num4 != 0)
+            {
+                num2 = (num2 << 5) + num2 ^ num4;
+                ch_ptr2 += 2;
+            }
+            else break;
         }
-        else break;
+        return num1 + num2 * 0x5D588B65;
     }
-    return num1 + num2 * 0x5D588B65;
-}
+};
+
+template<> struct hash_gen<String> {
+    static size_t getHashCode(const String& s) {
+        return hash_gen<char*>::getHashCode(s.c_str());
+    }
+};
+
+template<> struct hash_gen<wpointer> {
+    static size_t getHashCode(const wpointer m) {
+        return reinterpret_cast<size_t>(m);
+    }
+};
+
+template<> struct hash_gen<int> {
+    static size_t getHashCode(const int m) {
+        return static_cast<uint32_t>(m);
+    }
+};
