02ad57605ae5c9b3666be59d0f33741503afd3b7
Yuuki Wesp <ls-micro@ya.ru>
2021-01-27 07:42:41 +0300

    add keywords list

---
+++ compiler/stl/WaveKeywords.cs
@@ -0,0 +1,42 @@
+﻿namespace wave.stl
+{
+    using System.Collections.Generic;
+
+    public class WaveKeywords
+    {
+        public static List<string> list = new()
+        {
+            "public",
+            "private",
+            "protected",
+            "internal",
+            "static",
+            "readonly",
+            "operation",
+            "gc",
+            "auto",
+            "fail",
+            "sync",
+            "nocontrol",
+            "return",
+            "body",
+            "union",
+            "struct",
+            "extensions",
+            "where",
+            "when",
+            "as",
+            "is",
+            "new",
+            "const",
+            "global",
+            "if",
+            "else",
+            "while",
+            "do",
+            "for",
+            "foreach",
+            "extern"
+        };
+    }
+}
