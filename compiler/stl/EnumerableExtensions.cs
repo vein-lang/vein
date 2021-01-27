4ae1e6bde2d5cd2c4c848346524fcbcc049b753a
Yuuki Wesp <ls-micro@ya.ru>
2021-01-27 07:41:51 +0300

    add EnumerableExtensions

---
+++ compiler/stl/EnumerableExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace wave.stl
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class IEnumerableExtensions
+    {
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> enumerable) 
+            => enumerable == null || !enumerable.Any();
+
+        public static IEnumerable<T> EmptyIfNull<T>(this IEnumerable<T> enumerable) 
+            => enumerable ?? Enumerable.Empty<T>();
+
+        public static IEnumerable<T> OfExactType<T>(this IEnumerable enumerable) 
+            => enumerable.OfType<T>().Where(t => t.GetType() == typeof(T));
+    }
+}
