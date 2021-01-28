ebdd945c1cfec9d04e2b00ddfe282f03c9c4edc0
Yuuki Wesp <ls-micro@ya.ru>
2021-01-28 19:19:39 +0300

    add wave image read\write

---
+++ compiler/fs/WaveAssembly.cs
@@ -0,0 +1,99 @@
+﻿namespace wave.fs
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using BinaryTools.Elf;
+    using static BinaryTools.Elf.ElfSectionType;
+
+
+    public static class WEx
+    {
+        public static bool Exist(this ElfSectionHeaderTable table, ElfSectionType type) 
+            => table.Any(x => x.Type == type);
+
+        public static IEnumerator<int> GetEnumerator(this Range range) 
+            => Enumerable.Range(range.Start.Value, range.End.Value).GetEnumerator();
+    }
+
+    public partial class WaveAssembly
+    {
+        public string Name { get; init; }
+        
+        protected List<(string name, byte[] data)> sections { get; set; } = new ();
+        
+        public void AddSegment((string name, byte[] data) seg) => sections.Add(seg);
+
+        /// <exception cref="BadImageFormatException"/>
+        public static WaveAssembly LoadFromFile(string file)
+        {
+            using var fs = File.OpenRead(file);
+            using var br = new BinaryReader(fs);
+            var elf = ElfFile.ReadElfFile(br);
+
+            if (elf.Sections.All(x => x.Type != Note))
+                throw new BadImageFormatException($"File '{file}' has invalid.", 
+                    new ImageSegmentNotFoundException("elf .note segment not found."));
+            if (elf.Sections.All(x => x.Type != ProgBits))
+                throw new BadImageFormatException($"File '{file}' has invalid.", 
+                    new ImageSegmentNotFoundException("elf .progBits segment not found."));
+            
+            var noteSection = elf.Sections.Single(x => x is { Type: Note });
+            var keyCode = Encoding.ASCII.GetString(noteSection.ReadFrom(fs));
+            
+            if (keyCode != "wave")
+                throw new BadImageFormatException($"File '{file}' is not wave image.");
+            
+            
+            var ilCodeSection = elf.Sections.Single(x => x is { Type: ProgBits });
+
+            using var memory = new MemoryStream(ilCodeSection.ReadFrom(fs));
+            using var reader = new BinaryReader(memory);
+
+
+            var sectionCount = reader.ReadInt32();
+            var sections = new List<(string name, byte[] body)>();
+            foreach (var i in ..sectionCount)
+            {
+                var len = reader.ReadInt32();
+                var bytes = reader.ReadBytes(len);
+                var str = Encoding.ASCII.GetString(bytes);
+                sections.Add((str, null));
+            }
+
+            foreach (var i in ..sectionCount)
+            {
+                var tmp = sections[i];
+                sections[i] = (tmp.name, reader.ReadBytes(reader.ReadInt32()));
+            }
+            
+            return new WaveAssembly {sections = sections};
+        }
+        
+        public static void WriteToFile(WaveAssembly asm, string file)
+        {
+            using var memory = new MemoryStream();
+            using var writer = new BinaryWriter(memory);
+            
+            writer.Write(asm.sections.Count);
+            foreach (var (name, _) in asm.sections)
+            {
+                var b = Encoding.ASCII.GetBytes(name);
+                writer.Write(b.Length);
+                writer.Write(b);
+            }
+
+            foreach (var (_, data) in asm.sections)
+            {
+                writer.Write(data.Length);
+                writer.Write(data);
+            }
+
+            using var fs = File.Create(file);
+            
+            WriteElf(memory.ToArray(), fs);
+        }
+    }
+}
---
+++ compiler/fs/WaveAssembly.elf.cs
@@ -0,0 +1,152 @@
+namespace wave.fs
+{
+    using System.IO;
+    using System.Text;
+    using elf;
+    using ElfFile = elf.ElfFile;
+    using ElfHeader = elf.ElfHeader;
+    using ElfSection = elf.ElfSection;
+    using ElfSectionFlags = elf.ElfSectionFlags;
+    using ElfSectionType = elf.ElfSectionType;
+    using ElfSegment = elf.ElfSegment;
+    using ElfSegmentFlags = elf.ElfSegmentFlags;
+    using ElfSegmentType = elf.ElfSegmentType;
+    using ElfType = elf.ElfType;
+    public partial class WaveAssembly
+    {
+        protected internal static void WriteElf(byte[] ilCode, Stream stream)
+        {
+            using var writer = new BinaryWriter(stream);
+
+            var file = new ElfFile();
+            file.Sections.Add(new ElfSection());
+            AddCode(file, ilCode);
+
+            var sectionsStringsIndex = file.AddStringsSection();
+
+            const int headerSize = 0x34;
+            const int segmentsOffset = headerSize;
+            const int segmentEntrySize = 0x20;
+            var dataOffset = (uint)(segmentsOffset + file.Segments.Count * segmentEntrySize);
+            var sectionsOffset = dataOffset + file.Data.Length;
+            
+            
+            var header = new ElfHeader
+            {
+                Identification = {
+                    Magic = new[] { (char)0x7f, 'E', 'L', 'F' },
+                    FileClass = ElfFileClass.Elf32,
+                    DataType = ElfDataType.Lsb,
+                    Version = 1,
+                },
+                Type = ElfType.Executable,
+                Machine = 0x0,
+                Version = 1,
+                Entry = 0x0,
+                ProgramHeaderOffset = segmentsOffset,
+                SectionHeaderOffset = (uint)sectionsOffset,
+                Flags = 0x84,
+                ElfHeaderSize = headerSize,
+                ProgramHeaderEntrySize = segmentEntrySize,
+                ProgramHeaderCount = (ushort)file.Segments.Count,
+                SectionHeaderEntrySize = 0x28,
+                SectionHeaderCount = (ushort)file.Sections.Count,
+                StringSectionIndex = (ushort)sectionsStringsIndex
+            };
+            writer.WriteElf32(header);
+            foreach (var segment in file.Segments)
+            {
+                var cloned = segment;
+                cloned.Offset += dataOffset;
+                writer.WriteElf32(cloned);
+            }
+            writer.Write(file.Data.ToArray());
+            foreach (var section in file.Sections)
+            {
+                var cloned = section;
+                if (section.Type != ElfSectionType.Null)
+                {
+                    cloned.Offset += dataOffset;
+                }
+                writer.WriteElf32(cloned);
+            }
+        }
+        
+        private static void AddCode(ElfFile file, byte[] il)
+        {
+            file.Sections.Add(new ElfSection
+            {
+                Name = file.Strings.SaveString(".il_code"),
+                Type = ElfSectionType.ProgBits,
+                Address = 0,
+                Flags = ElfSectionFlags.Alloc | ElfSectionFlags.Executable,
+                Size = (uint)il.Length,
+                Align = 2,
+                Offset = (uint)file.Data.Position
+            });
+            file.Segments.Add(new ElfSegment
+            {
+                Type = ElfSegmentType.Load,
+                Offset = (uint)file.Data.Position,
+                VirtualAddress = 0,
+                PhysicalAddress = 0,
+                FileSize = (uint)il.Length,
+                MemorySize = (uint)il.Length,
+                Flags = ElfSegmentFlags.Executable | ElfSegmentFlags.Readable,
+                Align = 1
+            });
+            file.Data.Write(il, 0, il.Length);
+            
+            var vm_notes = Encoding.ASCII.GetBytes("wave");
+            file.Sections.Add(new ElfSection
+            {
+                Name = file.Strings.SaveString(".note"),
+                Type = ElfSectionType.Note,
+                Address = 0,
+                Flags = ElfSectionFlags.Alloc | ElfSectionFlags.Writeable,
+                Size = (uint)vm_notes.Length,
+                Align = 1,
+                Offset = (uint)file.Data.Position
+            });
+            file.Segments.Add(new ElfSegment
+            {
+                Type = ElfSegmentType.Note,
+                Offset = (uint)file.Data.Position,
+                VirtualAddress = 0,
+                PhysicalAddress = 0,
+                FileSize = (uint)vm_notes.Length,
+                MemorySize = (uint)vm_notes.Length,
+                Flags = ElfSegmentFlags.Readable,
+                Align = 1
+            });
+            file.Data.Write(vm_notes, 0, vm_notes.Length);
+        }
+
+        private void AddData(ElfFile file)
+        {
+            var code = Encoding.UTF8.GetBytes("the big string data; also foo string");
+            file.Sections.Add(new ElfSection
+            {
+                Name = file.Strings.SaveString(".bss"),
+                Type = ElfSectionType.NoBits,
+                Address = 0x800060,
+                Flags = ElfSectionFlags.Alloc | ElfSectionFlags.Writeable,
+                Size = (uint)code.Length,
+                Align = 1,
+                Offset = (uint)file.Data.Position
+            });
+            file.Segments.Add(new ElfSegment
+            {
+                Type = ElfSegmentType.Load,
+                Offset = (uint)file.Data.Position,
+                VirtualAddress = 0x800060,
+                PhysicalAddress = 0x800060,
+                FileSize = (uint)code.Length,
+                MemorySize = (uint)code.Length,
+                Flags = ElfSegmentFlags.Writeable | ElfSegmentFlags.Readable,
+                Align = 1,
+            });
+            file.Data.Write(code, 0, code.Length);
+        }
+    }
+}
