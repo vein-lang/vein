245cf519cb195fe02c3cdee3a1d003278f6dcb37
Yuuki Wesp <ls-micro@ya.ru>
2021-02-06 01:48:02 +0300

    mv exception

---
+++ lib/runtime/emit/FieldIsNotDeclaredException.cs
@@ -0,0 +1,12 @@
+﻿namespace wave.emit
+{
+    using System;
+
+    public class FieldIsNotDeclaredException : Exception
+    {
+        public FieldIsNotDeclaredException(FieldName field) : base($"Field '{field.name}' is not declared.")
+        {
+            
+        }
+    }
+}
