922c5791155440dfbb6ba98807fe7d48c064240d
Yuuki Wesp <ls-micro@ya.ru>
2021-02-08 00:08:53 +0300

    add func calle syntax

---
+++ compiler/syntax/types/IvocationSyntax.cs
@@ -0,0 +1,20 @@
+﻿namespace wave.syntax
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using stl;
+
+    public class InvocationExpressionSyntax : ExpressionSyntax
+    {
+        public override SyntaxType Kind => SyntaxType.InvocationExpression;
+        
+        public string FunctionName { get; set; }
+        public string[] MemberChain { get; set; }
+
+        public override IEnumerable<BaseSyntax> ChildNodes => GetNodes(Expression).Concat(Arguments.EmptyIfNull());
+
+        public ExpressionSyntax Expression { get; set; }
+
+        public List<ExpressionSyntax> Arguments { get; set; } = new();
+    }
+}
