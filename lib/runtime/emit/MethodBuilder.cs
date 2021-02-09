a8b32d6e2ce1ab53050c14342689c6ded5eef261
Yuuki Wesp <ls-micro@ya.ru>
2021-02-09 04:13:20 +0300

    fix external function compiling

--- lib/runtime/emit/MethodBuilder.cs
+++ lib/runtime/emit/MethodBuilder.cs
@@ -12,13 +12,15 @@
         internal readonly ClassBuilder classBuilder;
         private readonly string _name;
         private MethodFlags _flags;
+        private readonly WaveType _returnType;
         private readonly ILGenerator _generator;
         private readonly List<WaveArgumentRef> _args = new();
         private readonly Dictionary<int, WaveArgumentRef> _locals = new();
 
-        internal MethodBuilder(ClassBuilder clazz, string name, params WaveArgumentRef[] args)
+        internal MethodBuilder(ClassBuilder clazz, string name, WaveType returnType, params WaveArgumentRef[] args)
         {
             classBuilder = clazz;
+            _returnType = returnType;
             _name = name;
             _generator = new ILGenerator(this);
             this._args.AddRange(args);
@@ -31,11 +33,20 @@
         
         internal byte[] BakeByteArray()
         {
-            if (_generator.ILOffset == 0 && !_flags.HasFlag(MethodFlags.Extern))
-                return null;
             var idx = classBuilder.moduleBuilder.GetStringConstant(_name);
             using var mem = new MemoryStream();
             using var binary = new BinaryWriter(mem);
+            if (_flags.HasFlag(MethodFlags.Extern))
+            {
+                binary.Write(idx); // $method name
+                binary.Write((byte)_flags); // $flags
+                binary.Write(0); // body size
+                binary.Write((byte)0); // stack size TODO
+                binary.Write((byte)0); // locals size TODO
+                binary.Write(new byte[0]); // IL Body
+                return mem.ToArray();
+            }
+            
             var body = _generator.BakeByteArray();
             
             binary.Write(idx); // $method name
@@ -50,11 +61,15 @@
         
         internal string BakeDebugString()
         {
-            if (_generator.ILOffset == 0 && !_flags.HasFlag(MethodFlags.Extern))
-                return "<empty>";
-            var body = _generator.BakeDebugString();
             var str = new StringBuilder();
-            var args = _args.Select(x => $"{x.Name}: {x.Type.GetTypeName()}").Join(',');
+            var args = _args.Select(x => $"{x.Name}: {x.Type.Name}").Join(',');
+            if (_flags.HasFlag(MethodFlags.Extern))
+            {
+                str.AppendLine($".method extern {_name} ({args}) {_flags.EnumerateFlags().Join(' ').ToLowerInvariant()};");
+                return str.ToString();
+            }
+            var body = _generator.BakeDebugString();
+            
             str.AppendLine($".method {_name} ({args}) {_flags.EnumerateFlags().Join(' ').ToLowerInvariant()}");
             str.AppendLine("{");
             str.AppendLine($"\t.size {_generator.ILOffset}");
