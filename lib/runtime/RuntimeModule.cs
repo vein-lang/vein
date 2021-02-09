df625824804a73c2a2b1ddf79db60ed9256a819c
Yuuki Wesp <ls-micro@ya.ru>
2021-02-09 04:12:38 +0300

    add runtime module init

---
+++ lib/runtime/RuntimeModule.cs
@@ -0,0 +1,14 @@
+﻿namespace wave
+{
+    using System.Runtime.CompilerServices;
+    using emit;
+
+    public static class RuntimeModule
+    {
+        [ModuleInitializer]
+        public static void Init()
+        {
+            WaveCore.Init();
+        }
+    }
+}
