614b3035aa17ddc404cb17a07ce82194b48cecaa
Yuuki Wesp <ls-micro@ya.ru>
2021-02-11 01:34:24 +0300

    rework WaveClass

--- lib/runtime/emit/WaveClass.cs
+++ lib/runtime/emit/WaveClass.cs
@@ -4,33 +4,34 @@
 
     public class WaveClass
     {
-        public WaveType Type { get; set; }
-        public string Name { get; set; }
-        public string Path { get; set; }
+        public TypeName FullName { get; set; }
+        public string Name => FullName.Name;
+        public string Path => FullName.Namespace;
         public RuntimeToken Token { get; set; }
+        public ClassFlags Flags { get; set; }
         public WaveClass Parent { get; set; }
-        public readonly List<WaveClassField> Fields = new();
-        public readonly List<WaveClassMethod> Methods = new();
-
-        public WaveClass() { }
-
-        public WaveClass(WaveType name, WaveClass parent)
+        public readonly List<WaveField> Fields = new();
+        public readonly List<WaveMethod> Methods = new();
+        public WaveTypeCode TypeCode { get; set; } = WaveTypeCode.TYPE_CLASS;
+        
+        public WaveClass(TypeName name, WaveClass parent)
         {
-            this.Path = name.Namespace;
-            this.Name = name.Name;
-            this.Token = name.FullName.Token;
+            this.FullName = name;
+            this.Token = name.Token;
+            this.Parent = parent;
+        }
+        public WaveClass(WaveType type, WaveClass parent)
+        {
+            this.FullName = type.FullName;
+            this.Token = type.FullName.Token;
             this.Parent = parent;
         }
         
+        protected WaveClass() {  }
+        
         public WaveClass DefineMethod(string name, WaveType returnType, MethodFlags flags, params WaveArgumentRef[] args)
         {
-            var method = new WaveClassMethod
-            {
-                Flags = flags,
-                Name = name,
-                ReturnType = returnType,
-                Owner = this
-            };
+            var method = new WaveMethod(name, flags, returnType, this, args);
             method.Arguments.AddRange(args);
             Methods.Add(method);
             return this;
