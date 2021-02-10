6523a42ab3b190feeb3b572857471bc0b75aa12f
Yuuki Wesp <ls-micro@ya.ru>
2021-02-11 01:26:08 +0300

    add GetEnumerable for range

--- lib/runtime/extensions/EnumExtension.cs
+++ lib/runtime/extensions/EnumExtension.cs
@@ -1,5 +1,4 @@
-﻿[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("wc_test")]
-namespace wave.extensions
+﻿namespace wave
 {
     using System;
     using System.Collections.Generic;
@@ -18,15 +17,13 @@ namespace wave.extensions
             return Enum.GetValues(typeof(TEnum)).Cast<Enum>().Where(x => buff.HasFlag(x)).Cast<TEnum>();
         }
 
+        public static IEnumerable<int> GetEnumerable(this Range i) =>
+            Enumerable.Range(i.Start.Value, i.End.Value);
+        public static IEnumerator<int> GetEnumerator(this Range i) =>
+            Enumerable.Range(i.Start.Value, i.End.Value).GetEnumerator();
         public static bool InRange(this Range range, int value) 
             => value >= range.Start.Value && value <= range.End.Value;
         public static bool InRange(this OpCode opcode, OpCodeValue start, OpCodeValue end)
             => ((ushort) start..(ushort) end).InRange(opcode.Value);
-    }
-    
-    public static class EnumerableExtension
-    {
-        public static string Join<T>(this IEnumerable<T> t, char joint) => string.Join(joint, t);
-        public static string Join<T>(this IEnumerable<T> t, string joint) => string.Join(joint, t);
     }
 }
