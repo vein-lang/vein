7a254a18df408f35cada244d8b7ea095324ffd1d
Yuuki Wesp <ls-micro@ya.ru>
2021-02-11 01:37:10 +0300

    fix Name

--- lib/runtime/emit/FieldIsNotDeclaredException.cs
+++ lib/runtime/emit/FieldIsNotDeclaredException.cs
@@ -4,7 +4,7 @@
 
     public class FieldIsNotDeclaredException : Exception
     {
-        public FieldIsNotDeclaredException(FieldName field) : base($"Field '{field.name}' is not declared.")
+        public FieldIsNotDeclaredException(FieldName field) : base($"Field '{field.Name}' is not declared.")
         {
             
         }
