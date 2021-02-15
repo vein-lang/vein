9f3b55445985e885be6ce203f424a35488658b8c
Yuuki Wesp <ls-micro@ya.ru>
2021-02-16 02:09:52 +0300

    add InvalidOperationException

--- include/emit/Exceptions.hpp
+++ include/emit/Exceptions.hpp
@@ -5,3 +5,4 @@ CUSTOM_EXCEPTION(AggregateException);
 CUSTOM_EXCEPTION(TypeNotFoundException);
 CUSTOM_EXCEPTION(SequenceContainsNoElements);
 CUSTOM_EXCEPTION(NotImplementedException);
+CUSTOM_EXCEPTION(InvalidOperationException);
