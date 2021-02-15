b9e078c7715d65af3bbefc5d2f7b2dbd147cf8f4
Yuuki Wesp <ls-micro@ya.ru>
2021-02-16 01:49:18 +0300

    add ClassFlags

---
+++ include/emit/ClassFlags.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+enum ClassFlags
+{
+    None        = 0 << 0,
+    Public      = 1 << 0,
+    Static      = 1 << 1,
+    Internal    = 1 << 2,
+    Protected   = 1 << 3,
+    Private     = 1 << 4
+};
