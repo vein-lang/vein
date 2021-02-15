26b70068aea9e5e420725cdecfacbf33575093ea
Yuuki Wesp <ls-micro@ya.ru>
2021-02-16 02:15:42 +0300

    rework class

---
+++ include/types/WaveClass.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#include "WaveMethod.h"
+#include "WaveType.hpp"
+#include "collections/list.hpp"
+#include "emit/ClassFlags.hpp"
+#include "emit/TypeName.hpp"
+#include "emit/WaveField.hpp"
+
+class WaveClass {
+public:
+	TypeName* FullName;
+
+	string GetName() { return FullName->get_name(); }
+    string GetPath()  { return FullName->get_namespace(); }
+
+	ClassFlags Flags;
+    WaveClass* Parent;
+
+	list<WaveField>* Fields = new list<WaveField>();
+    list<WaveMethod>* Methods = new list<WaveMethod>();
+
+
+	WaveTypeCode TypeCode = TYPE_CLASS;
+
+
+    WaveClass(TypeName* name, WaveClass* parent)
+    {
+        Parent = parent;
+        Flags = CLASS_None;
+        FullName = name;
+    }
+
+    WaveClass(WaveType* type, WaveClass* parent)
+    {
+        Parent = parent;
+        Flags = CLASS_None;
+        FullName = type->get_full_name();
+    }
+};
