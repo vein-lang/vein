980f7da695886f4a53b5c6591c8f49ac1c9d7ac7
Yuuki Wesp <ls-micro@ya.ru>
2021-02-19 03:59:53 +0300

    rework WaveMethod

--- include/types/WaveMethod.hpp
+++ include/types/WaveMethod.hpp
@@ -4,14 +4,74 @@
 #include "WaveMethodHeader.hpp"
 #include "WaveMethodPInvokeInfo.hpp"
 #include "WaveMethodSignature.hpp"
+#include "collections/list_t.hpp"
+#include "emit/MethodFlags.hpp"
+#include "emit/WaveMember.hpp"
 
-typedef struct {
-	const char*             name;
-	uint16_t                flags;
-	WaveMethodSignature*    signature;
-	WaveImage*              image;
+class WaveArgumentRef;
+using namespace std;
+
+class WaveMethodBase : WaveMember
+{
+protected:
+    WaveMethodBase(const wstring& name, MethodFlags flags, list_t<WaveArgumentRef*>* args)
+    : WaveMember(name)
+    {
+        Arguments = args;
+
+    }
+public:
+	MethodFlags Flags;
+	list_t<WaveArgumentRef*>* Arguments;
+
+	int ArgLen() const noexcept
+    {
+	    if (Arguments == nullptr)
+			return 0;
+		return Arguments->size();
+	}
+	WaveMemberKind GetKind() override
+	{
+	    return WaveMemberKind::Method;
+	}
+
+	[[nodiscard]]
+	virtual bool IsStatic() const noexcept
+	{
+	    return (Flags & MethodStatic) != 0;
+	}
+
+	[[nodiscard]]
+	virtual bool IsPrivate() const noexcept
+	{
+	    return (Flags & MethodPrivate) != 0;
+	}
+
+    [[nodiscard]]
+    virtual bool IsExtern() const noexcept
+	{
+	    return (Flags & MethodExtern) != 0;
+	}
+};
+
+class WaveMethod : WaveMethodBase
+{
+public:
+	WaveType* ReturnType;
+	WaveClass* Owner;
+	unsigned char StackSize; 
+	unsigned char LocalsSize; 
+
 	union {
 		MetaMethodHeader* header;
 		WaveMethodPInvokeInfo* piinfo;
 	} data;
-} WaveMethod;
+
+    WaveMethod(const wstring& name,
+		MethodFlags flags, WaveType* retType,
+		WaveClass* owner, list_t<WaveArgumentRef*>* args) : WaveMethodBase(name, flags, args)
+    {
+        ReturnType = retType;
+		Owner = owner;
+    }
+};
