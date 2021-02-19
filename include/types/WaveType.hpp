106b950f5415b0dfe0b70400d3a28249afdc5552
Yuuki Wesp <ls-micro@ya.ru>
2021-02-19 04:00:03 +0300

    add WaveTypeImpl

--- include/types/WaveType.hpp
+++ include/types/WaveType.hpp
@@ -3,7 +3,7 @@
 
 
 #include "WaveTypeCode.hpp"
-#include "collections/list.hpp"
+#include "collections/list_t.hpp"
 #include "emit/ClassFlags.hpp"
 #include "emit/TypeName.hpp"
 #include "emit/WaveMember.hpp"
@@ -13,30 +13,94 @@ using namespace std;
 class WaveType : WaveMember
 {
 public:
-    virtual string get_namespace() = 0;
-    list<WaveMember>* Members = new list<WaveMember>();
+    virtual wstring get_namespace()
+    {
+        return get_full_name()->get_namespace();
+    }
+    list_t<WaveMember*>* Members = new list_t<WaveMember*>();
     WaveType* Parent;
     ClassFlags classFlags;
     WaveTypeCode TypeCode;
 
     virtual TypeName* get_full_name() = 0;
 
+    WaveMemberKind GetKind() override
+    {
+        return WaveMemberKind::Type;
+    }
 
-    virtual bool IsArray() { return false; }
-    virtual bool IsSealed() { return false; }
-    virtual bool IsClass() { return false; }
-    virtual bool IsPublic() { return false; }
-    virtual bool IsPrimitive() { return false; }
-    bool IsPrivate() { return !IsPublic(); }
+
+    [[nodiscard]] virtual bool IsArray    () const noexcept { return false; }
+    [[nodiscard]] virtual bool IsSealed   () const noexcept { return false; }
+    [[nodiscard]] virtual bool IsClass    () const noexcept { return false; }
+    [[nodiscard]] virtual bool IsPublic   () const noexcept { return false; }
+    [[nodiscard]] virtual bool IsPrimitive() const noexcept { return false; }
+    [[nodiscard]]         bool IsPrivate  () const noexcept { return !IsPublic(); }
 
 
     static WaveType* ByName(TypeName* name) { throw NotImplementedException(); }
 
 protected:
-    WaveType(const string& name, WaveType* parent) : WaveMember(name)
+    WaveType(const wstring& name, WaveType* parent) : WaveMember(name)
     {
         Parent = parent;
         TypeCode = TYPE_CLASS;
         classFlags = CLASS_None;
     }
+    WaveType() : WaveMember(nullptr)
+    {
+        Parent = nullptr;
+        classFlags = CLASS_None;
+    }
+};
+
+class WaveTypeImpl : WaveType
+{
+    
+public:
+    WaveTypeImpl(TypeName* tn, WaveTypeCode code) : WaveType()
+    {
+        type_name_ = tn;
+        TypeCode = code;
+    }
+    WaveTypeImpl(TypeName* tn, WaveTypeCode code, ClassFlags flags) : WaveTypeImpl(tn, code)
+    {
+        classFlags = flags;
+    }
+    WaveTypeImpl(TypeName* tn, WaveTypeCode code, ClassFlags flags, WaveType* parent)
+    : WaveTypeImpl(tn, code, flags)
+    {
+        Parent = parent;
+    }
+    [[nodiscard]]
+    bool IsArray() const noexcept override
+    {
+        return false;
+    }
+    [[nodiscard]]
+    bool IsSealed() const noexcept override
+    {
+        return false;
+    }
+    [[nodiscard]]
+    bool IsClass() const noexcept override
+    {
+        return !IsPrimitive();
+    }
+    [[nodiscard]]
+    bool IsPublic() const noexcept override
+    {
+        return (classFlags & CLASS_Public) != 0;
+    }
+    [[nodiscard]]
+    bool IsPrimitive() const noexcept override
+    {
+        return TypeCode != TYPE_CLASS;
+    }
+    TypeName* get_full_name() override
+    {
+        return type_name_;
+    }
+private:
+    TypeName* type_name_;
 };
