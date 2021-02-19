13f8cc88ab1688a9bc7e8153c6feafae3660b8c4
Yuuki Wesp <ls-micro@ya.ru>
2021-02-19 03:58:26 +0300

    refactoring Field Name

--- include/emit/FieldName.hpp
+++ include/emit/FieldName.hpp
@@ -6,31 +6,31 @@
 using namespace std;
 struct FieldName
 {
-    string FullName;
-    [[nodiscard]] string get_name()
+    wstring FullName;
+    [[nodiscard]] wstring get_name() const
     {
         auto results = split(FullName, '.');
         auto result = results[results.size() - 1];
         results.clear();
         return result;
     }
-    [[nodiscard]] string GetClass()
+    [[nodiscard]] wstring GetClass() const
     {
         auto n = get_name();
         return FullName.substr(0, (FullName.size() - n.size()) - 1);
     }
 
-    FieldName(const string& n, const string& c) noexcept(true)
+    FieldName(const wstring& n, const wstring& c) noexcept(true)
     {
-        FullName = n + "." + c;
+        FullName = n + L"." + c;
     }
     [[nodiscard]] static FieldName* construct(const int nameIdx, const int classIdx, GetConstByIndexDelegate* m) noexcept(true)
     {
-        return new FieldName(m(nameIdx), m(classIdx));
+        return new FieldName(m->operator()(nameIdx), m->operator()(classIdx));
     }
     [[nodiscard]] static FieldName* construct(const long idx, GetConstByIndexDelegate* m) noexcept(true)
     {
-        return new FieldName(m(static_cast<int>(idx >> 32)), 
-            m(static_cast<int>(idx & static_cast<uint32_t>(4294967295))));
+        return new FieldName(m->operator()(static_cast<int>(idx >> 32)), 
+            m->operator()(static_cast<int>(idx & static_cast<uint32_t>(4294967295))));
     }
 };
