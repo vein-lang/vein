8e981ed57c2c6a1253c36aceace66abb7c2e1164
Yuuki Wesp <ls-micro@ya.ru>
2021-02-20 16:57:14 +0300

    add equality for TypeName, use int64_t

--- include/emit/TypeName.hpp
+++ include/emit/TypeName.hpp
@@ -26,14 +26,23 @@ struct TypeName
         FullName = n + L"/" + c;
     }
 
+    TypeName(const wstring& fullName) noexcept(true)
+    {
+        FullName = fullName;
+    }
 
     [[nodiscard]] static TypeName* construct(const int nameIdx, const int classIdx, GetConstByIndexDelegate* m) noexcept(true)
     {
         return new TypeName(m->operator()(nameIdx), m->operator()(classIdx));
     }
-    [[nodiscard]] static TypeName* construct(const long idx, GetConstByIndexDelegate* m) noexcept(true)
+    [[nodiscard]] static TypeName* construct(const int64_t idx, GetConstByIndexDelegate* m) noexcept(true)
     {
-        return new TypeName(m->operator()(static_cast<int>(idx >> 32)), 
-            m->operator()(static_cast<int>(idx & static_cast<uint32_t>(4294967295))));
+        return new TypeName(m->operator()(static_cast<int>(idx & static_cast<uint32_t>(4294967295))), 
+            m->operator()(static_cast<int>(idx >> 32)));
     }
+};
+template<> struct equality<TypeName*> {
+	static bool equal(TypeName* l, TypeName* r) {
+		return wcscmp(l->FullName.c_str(), r->FullName.c_str()) == 0;
+	}
 };
