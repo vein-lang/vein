1066e59927b9815b59e51993f670af7119f31031
Yuuki Wesp <ls-micro@ya.ru>
2021-02-20 16:59:18 +0300

    add default types, add native_class, string_class

--- include/types/WaveCore.hpp
+++ include/types/WaveCore.hpp
@@ -1,16 +1,25 @@
 #pragma once
 #include "WaveClass.hpp"
-#include "WaveImage.hpp"
+#include "emit/WaveModue.hpp"
 
 typedef struct
 {
-    WaveImage* corlib;
+    WaveModule* corlib;
     WaveClass* object_class;
     WaveClass* i1_class;
     WaveClass* i2_class;
     WaveClass* i4_class;
     WaveClass* i8_class;
     WaveClass* void_class;
+    WaveClass* native_class;
+    WaveClass* string_class;
+
+
+    WaveType* object_type;
+    WaveType* void_type;
+    WaveType* value_type;
+    WaveType* native_type;
+    WaveType* string_type;
 } WaveCore;
 
 static WaveCore* wave_core = new WaveCore();
