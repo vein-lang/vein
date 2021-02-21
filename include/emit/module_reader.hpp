af1623ee10a914d176ab3ebbad4cfe3d39b9c494
Yuuki Wesp <ls-micro@ya.ru>
2021-02-21 07:57:39 +0300

    add decodeIL

--- include/emit/module_reader.hpp
+++ include/emit/module_reader.hpp
@@ -6,6 +6,66 @@
 #include "streams/memory_stream.hpp"
 #include "streams/binary_reader.hpp"
 
+struct ILWrapper
+{
+    uint32_t* code;
+    uint32_t size;
+};
+
+auto decodeIL(BinaryReader* reader) noexcept(false)
+{
+    auto* result = new list_t<uint32_t>();
+    
+    while (reader->Position() < reader->Length())
+    {
+        auto opcode = reader->ReadByte();
+        if (opcode == NOP)
+            continue;
+        auto opcode_name = opcodes[opcode];
+        printf("\t\t\t\t\t[ .%s, %d ]\n", opcode_name, opcode);
+        if (opcode >= LAST)
+            throw CorruptILException(fmt::format(L"OpCode '{0}' is not found in metadata.\n{1}", 
+                                                 opcode, L"re-run 'gen.csx' for fix this error."));
+        result->push_back(opcode);
+        auto size = static_cast<int>(opcode_size[opcode]);
+        switch (size)
+        {
+            case 0:
+                continue;
+            case sizeof(char):
+                result->push_back(reader->ReadByte());
+                continue;
+            case sizeof(int16_t):
+                result->push_back(reader->Read2());
+                continue;
+            case sizeof(int32_t):
+                result->push_back(reader->Read4());
+                continue;
+            case sizeof(int64_t):
+                result->push_back(reader->Read4());
+                result->push_back(reader->Read4());
+                continue;
+            case 16:
+                result->push_back(reader->Read4());
+                result->push_back(reader->Read4());
+                result->push_back(reader->Read4());
+                result->push_back(reader->Read4());
+                continue;
+            default:
+                throw CorruptILException(fmt::format(L"OpCode '{0}' has invalid size [{1}].\n{2}", opcode, size,
+                        L"Check 'opcodes.def' and re-run 'gen.csx' for fix this error."));
+        }
+    }
+    const ILWrapper il {
+         new uint32_t[result->size()],
+         static_cast<uint32_t>(result->size())
+    };
+    for (auto i = 0u; i != il.size; i++)
+        il.code[i] = result->at(i);
+    //delete result;
+    return il;
+}
+
 
 [[nodiscard]]
 auto* readArguments(BinaryReader* reader, WaveModule* m) noexcept
@@ -39,10 +99,10 @@ WaveMethod* readMethod(BinaryReader* reader, WaveClass* clazz, WaveModule* m) no
     };
 
     const auto idx = reader->Read4();
-    const auto flags = static_cast<MethodFlags>(reader->ReadU2());
+    const auto flags = static_cast<MethodFlags>(reader->ReadByte());
     const auto bodySize = reader->Read4();
-    const auto stackSize = reader->ReadU2();
-    const auto locals = reader->ReadU2();
+    const auto stackSize = reader->ReadByte();
+    const auto locals = reader->ReadByte();
     const auto retTypeIdx = reader->Read8();
     auto* args = readArguments(reader, m);
     auto* body = reader->ReadBytes(bodySize);
@@ -53,6 +113,12 @@ WaveMethod* readMethod(BinaryReader* reader, WaveClass* clazz, WaveModule* m) no
 
     auto* method = new WaveMethod(name, flags, retType, clazz, args);
 
+    printf("\t\t\t\t{\n");
+    printf("\t\t\t\tidx: %d, stack: %d, locals: %d\n", idx, bodySize, stackSize, locals);
+    printf("\t\t\t\tmethod name: %ws\n", name.c_str());
+    printf("\t\t\t\treturn type: %ws\n", retTypeName->FullName.c_str());
+    printf("\t\t\t\treturn type: %ws\n", retTypeName->FullName.c_str());
+
     method->LocalsSize = locals;
     method->StackSize = stackSize;
 
@@ -60,14 +126,23 @@ WaveMethod* readMethod(BinaryReader* reader, WaveClass* clazz, WaveModule* m) no
     {
         method->data.piinfo = new WaveMethodPInvokeInfo();
         method->data.piinfo->addr = nullptr; // TODO
+        printf("\t\t\t\t\t[native code]\n");
     }
     else
     {
         method->data.header = new MetaMethodHeader();
-        method->data.header->code_size = bodySize;
+
+        auto* const ilMem = new MemoryStream(body, bodySize);
+        auto* const ilReader = new BinaryReader(ilMem);
+
+        auto const il = decodeIL(ilReader);
+
+        method->data.header->code_size = il.size;
         method->data.header->max_stack = method->StackSize;
-        //method->data.header->code = body; TODO
+        method->data.header->code = &*il.code;
+        //delete ilMem;
     }
+    printf("\t\t\t\t},\n");
     return method;
 }
 
@@ -76,10 +151,12 @@ WaveClass* readClass(BinaryReader* reader, WaveModule* m) noexcept(false)
 {
     const auto idx = reader->Read4();
     const auto nsidx = reader->Read4();
-    const auto flags = static_cast<ClassFlags>(reader->ReadU2());
+    const auto flags = static_cast<ClassFlags>(reader->ReadByte());
 
     const auto parentIdx = reader->Read8();
     const auto len = reader->Read4();
+
+    printf("\t\t\tidx: %d, ns_idx: %d\n", idx, nsidx);
 
     function<wstring(int z)> wn = [m](const int w) {
         return m->GetConstByIndex(w);
@@ -90,6 +167,9 @@ WaveClass* readClass(BinaryReader* reader, WaveModule* m) noexcept(false)
     auto* className = TypeName::construct(nsidx, idx, &wn);
     auto* clazz = new WaveClass(className, AsClass(parent));
     clazz->Flags = flags;
+    printf("\t\t\tclass name: '%ws'\n", typeName->FullName.c_str());
+    printf("\t\t\tclass parent: '%ws'\n", parent->get_full_name()->FullName.c_str());
+    printf("\t\t\t[construct method table] size: %d\n", len);
     for (auto i = 0; i != len; i++)
     {
         const auto size = reader->Read4();
@@ -98,9 +178,9 @@ WaveClass* readClass(BinaryReader* reader, WaveModule* m) noexcept(false)
         auto* const methodReader = new BinaryReader(methodMem);
         auto* const method = readMethod(methodReader, clazz, m);
         clazz->Methods->push_back(method);
-        delete methodReader;
-        delete methodMem;
-        delete body;
+        //delete methodReader;
+        //delete methodMem;
+        //delete body;
     }
 
     const auto fsize = reader->Read4();
@@ -111,7 +191,7 @@ WaveClass* readClass(BinaryReader* reader, WaveModule* m) noexcept(false)
         const auto tnameIdx = reader->Read8();
         auto* tname = TypeName::construct(tnameIdx, &wn);
         auto* const return_type = m->FindType(tname, true);
-        const auto fflags  = static_cast<FieldFlags>(reader->ReadU2());
+        const auto fflags  = static_cast<FieldFlags>(reader->ReadByte());
         //auto litVal = reader->ReadBytes(0);
         auto* field = new WaveField(clazz, fname, fflags, return_type);
         clazz->Fields->push_back(field);
@@ -130,28 +210,34 @@ WaveModule* readModule(char* arr, size_t sz, list_t<WaveModule*>* deps) noexcept
     target->deps = deps;
     
     const auto idx = reader->Read4();
-
     const auto ssize = reader->Read4();
+    printf("\t[construct const string table] size: %d\n", ssize);
     for (auto i = 0; i != ssize; i++)
     {
         auto key = reader->Read4();
         auto str = reader->ReadInsomniaString();
         target->strings->insert({key, str});
+        printf("\t\t{'%d'}: '%ws'\n", key, str.c_str());
     }
     const auto csize = reader->Read4();
+    printf("\t[construct class table] size: %d\n", csize);
     for (auto i = 0; i != csize; i++)
     {
         const auto size = reader->Read4();
         auto* body = reader->ReadBytes(size);
         auto* const classMem = new MemoryStream(body, size);
         auto* const classReader = new BinaryReader(classMem);
+        printf("\t\t{\n");
         auto* clazz = readClass(classReader, target);
+        printf("\t\t}\n;");
         target->classList->push_back(clazz);
-        delete classReader;
-        delete classMem;
-        delete body;
+        //delete classReader;
+        //delete classMem;
+        //delete body;
     }
-
     target->name = target->GetConstByIndex(idx);
+    printf("\tload '%ws' module success\n", target->name.c_str());
+
+    
     return target;
 }
