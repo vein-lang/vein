7e7b65576d055493f61ecaa148b698822ec3b3dd
Yuuki Wesp <ls-micro@ya.ru>
2021-02-21 03:01:01 +0300

    mv BytesToUTF8

--- include/compatibility.types.hpp
+++ include/compatibility.types.hpp
@@ -84,6 +84,43 @@ static inline wpointer malloc0(const uintptr_t x)
 #define init_serial()
 #endif
 
+
+static std::wstring BytesToUTF8(const char* in, size_t size)
+{
+    std::wstring out;
+    unsigned int codepoint;
+    int arrow = 0;
+    while (arrow != size)
+    {
+        unsigned char ch = static_cast<unsigned char>(*in);
+        if (ch <= 0x7f)
+            codepoint = ch;
+        else if (ch <= 0xbf)
+            codepoint = (codepoint << 6) | (ch & 0x3f);
+        else if (ch <= 0xdf)
+            codepoint = ch & 0x1f;
+        else if (ch <= 0xef)
+            codepoint = ch & 0x0f;
+        else
+            codepoint = ch & 0x07;
+        ++in;
+        ++arrow;
+        if (((*in & 0xc0) != 0x80) && (codepoint <= 0x10ffff))
+        {
+            if (sizeof(wchar_t) > 2)
+                out.append(1, static_cast<wchar_t>(codepoint));
+            else if (codepoint > 0xffff)
+            {
+                out.append(1, static_cast<wchar_t>(0xd800 + (codepoint >> 10)));
+                out.append(1, static_cast<wchar_t>(0xdc00 + (codepoint & 0x03ff)));
+            }
+            else if (codepoint < 0xd800 || codepoint >= 0xe000)
+                out.append(1, static_cast<wchar_t>(codepoint));
+        }
+    }
+    return out;
+}
+
 #define CUSTOM_EXCEPTION(name) struct name : public std::exception {   \
     const char* msg;                                                   \
     name() { msg = ""; }                                               \
@@ -93,7 +130,7 @@ static inline wpointer malloc0(const uintptr_t x)
 }
 #define CUSTOM_WEXCEPTION(name) struct name                        {   \
     const wchar_t* msg;                                                \
-    name() { msg = L""; }                                               \
+    name() { msg = L""; }                                              \
     name(const wchar_t* message) { msg = (message); }                  \
     name(std::wstring message  ) { msg = (message.c_str()); }          \
     _NODISCARD const wchar_t* what() const throw () { return msg; }    \
--- include/streams/binary_reader.hpp
+++ include/streams/binary_reader.hpp
@@ -53,7 +53,7 @@ public:
         const auto magic = ReadU2();
         if (magic != 0x45)
             throw InvalidOperationException("Cannot read string from binary stream. [magic flag invalid]");
-        const auto body = ReadBytes(size);
+        auto* const body = ReadBytes(size);
 
         return BytesToUTF8(body, size);
     }
@@ -63,41 +63,4 @@ public:
 
 private:
     MemoryStream* _origin;
-
-
-    static std::wstring BytesToUTF8(const char* in, size_t size)
-    {
-        std::wstring out;
-        unsigned int codepoint;
-        int arrow = 0;
-        while (arrow != size)
-        {
-            unsigned char ch = static_cast<unsigned char>(*in);
-            if (ch <= 0x7f)
-                codepoint = ch;
-            else if (ch <= 0xbf)
-                codepoint = (codepoint << 6) | (ch & 0x3f);
-            else if (ch <= 0xdf)
-                codepoint = ch & 0x1f;
-            else if (ch <= 0xef)
-                codepoint = ch & 0x0f;
-            else
-                codepoint = ch & 0x07;
-            ++in;
-            ++arrow;
-            if (((*in & 0xc0) != 0x80) && (codepoint <= 0x10ffff))
-            {
-                if (sizeof(wchar_t) > 2)
-                    out.append(1, static_cast<wchar_t>(codepoint));
-                else if (codepoint > 0xffff)
-                {
-                    out.append(1, static_cast<wchar_t>(0xd800 + (codepoint >> 10)));
-                    out.append(1, static_cast<wchar_t>(0xdc00 + (codepoint & 0x03ff)));
-                }
-                else if (codepoint < 0xd800 || codepoint >= 0xe000)
-                    out.append(1, static_cast<wchar_t>(codepoint));
-            }
-        }
-        return out;
-    }
 };
