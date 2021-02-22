1c6752e1e48bd0120c966787853973f102c0d970
Yuuki Wesp <ls-micro@ya.ru>
2021-02-22 06:40:27 +0300

    refactoring

--- include/emit/MethodFlags.hpp
+++ include/emit/MethodFlags.hpp
@@ -2,11 +2,11 @@
 
 enum MethodFlags : int32_t
 {
-        MethodPublic = 1 << 0,
-        MethodStatic = 1 << 1,
-        MethodInternal = 1 << 2,
-        MethodProtected = 1 << 3,
-        MethodPrivate = 1 << 4,
-        MethodExtern = 1 << 5,
-        MethodVirtual = 1 << 6
+    MethodPublic = 1 << 0,
+    MethodStatic = 1 << 1,
+    MethodInternal = 1 << 2,
+    MethodProtected = 1 << 3,
+    MethodPrivate = 1 << 4,
+    MethodExtern = 1 << 5,
+    MethodVirtual = 1 << 6
 };
