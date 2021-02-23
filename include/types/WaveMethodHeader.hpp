5529c9d00586a666b03d60ea5ac3ef0e7ef7d8a1
Yuuki Wesp <ls-micro@ya.ru>
2021-02-23 06:09:08 +0300

    add labels_map, labels

--- include/types/WaveMethodHeader.hpp
+++ include/types/WaveMethodHeader.hpp
@@ -1,5 +1,8 @@
 #pragma once
 #include "compatibility.types.hpp"
+#include "collections/dictionary.hpp"
+#include "collections/list_t.hpp"
+#include "emit/ILLabel.hpp"
 
 typedef struct {
 	uint32_t                code_size;
@@ -7,5 +10,7 @@ typedef struct {
 	short                   max_stack;
 	uint32_t                local_var_sig_tok;
 	unsigned int            init_locals : 1;
-	void* exception_handler_list;
+	void*                   exception_handler_list;
+	dictionary<int, ILLabel>*   labels_map;
+	list_t<int>*                labels;
 } MetaMethodHeader;
