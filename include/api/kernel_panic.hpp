11cba41d5924b6fa9701444f483ebc3d08ba2c64
Yuuki Wesp <ls-micro@ya.ru>
2021-02-28 17:18:15 +0300

    add native exception thrower

---
+++ include/api/kernel_panic.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+enum WaveNativeException
+{
+    WAVE_EXCEPTION_NONE = 0,
+    WAVE_EXCEPTION_MISSING_METHOD,
+    WAVE_EXCEPTION_MISSING_FIELD,
+    WAVE_EXCEPTION_TYPE_LOAD,
+    WAVE_EXCEPTION_MEMBER_ACCESS
+};
+
+static const wchar_t* wave_exception_names[] = {
+    L"WAVE_EXCEPTION_NONE",
+    L"WAVE_EXCEPTION_MISSING_METHOD",
+    L"WAVE_EXCEPTION_MISSING_FIELD",
+    L"WAVE_EXCEPTION_TYPE_LOAD",
+    L"WAVE_EXCEPTION_MEMBER_ACCESS"
+};
+
+struct NativeException
+{
+    WaveNativeException code;
+    wstring msg;
+};
+
+struct WaveKernelData
+{
+    NativeException* exception;
+};
+
+static inline WaveKernelData* kernel_data;
+
+
+inline void init_kernel_data()
+{
+    if (kernel_data == nullptr)
+        kernel_data = new WaveKernelData();
+}
+
+WaveKernelData* get_kernel_data()
+{
+    init_kernel_data();
+    return kernel_data;
+}
+
+
+
+inline void set_failure(WaveNativeException exceptionCode, const wstring& msg)
+{
+    init_kernel_data();
+
+    if (kernel_data->exception)
+        return;
+    kernel_data->exception = new NativeException {
+        exceptionCode, msg
+    };
+}
