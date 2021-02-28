01057a371023cef4983f4038be295cb13a594426
Yuuki Wesp <ls-micro@ya.ru>
2021-02-28 17:18:45 +0300

    add FIELD_Virtual, FIELD_Abstract, FIELD_Override

--- include/emit/FieldFlags.hpp
+++ include/emit/FieldFlags.hpp
@@ -6,5 +6,8 @@ enum FieldFlags
     FIELD_Literal = 1 << 1,
     FIELD_Public = 1 << 2,
     FIELD_Static = 1 << 3,
-    FIELD_Protected = 1 << 4
+    FIELD_Protected = 1 << 4,
+    FIELD_Virtual = 1 << 5,
+    FIELD_Abstract = 1 << 6,
+    FIELD_Override = 1 << 7
 };
