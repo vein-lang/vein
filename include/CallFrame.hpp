c3220e2258be1f8ece13bb8f1496f5ccc6ac59f0
Yuuki Wesp <ls-micro@ya.ru>
2021-03-03 19:55:37 +0300

    add fill_stacktrace, and field stack_trace

--- include/CallFrame.hpp
+++ include/CallFrame.hpp
@@ -9,6 +9,7 @@ struct CallFrameException
 {
     uint32_t* last_ip;
     WaveObject* value;
+    wstring stack_trace;
 };
 
 struct CallFrame
@@ -22,4 +23,22 @@ struct CallFrame
     int level;
 
     CallFrameException* exception;
+
+
+    static void fill_stacktrace(CallFrame* frame)
+    {
+        std::wstringstream s;
+
+        s << fmt::format(L"\tat {0}{1}.{2}\n", frame->method->Owner->FullName->get_namespace(), frame->method->Owner->FullName->get_name(), frame->method->Name);
+
+        auto r = frame->parent;
+
+        while (r != nullptr)
+        {
+            s << fmt::format(L"\tat {0}{1}.{2}\n", r->method->Owner->FullName->get_namespace(), r->method->Owner->FullName->get_name(), r->method->Name);
+            r = r->parent;
+        }
+
+        frame->exception->stack_trace = s.str();
+    }
 };
