44698010883bcdb78edbe8cf657d664f250cb8a3
Yuuki Wesp <ls-micro@ya.ru>
2021-03-03 22:56:29 +0300

    add ExceptionType

--- lib/runtime/emit/WaveCore.cs
+++ lib/runtime/emit/WaveCore.cs
@@ -20,7 +20,8 @@
                 DecimalType,
                 CharType,
                 BoolType,
-                ArrayType
+                ArrayType,
+                ExceptionType
             };
             public static WaveType ObjectType { get; private set; }
             public static WaveType ValueType { get; private set; }
@@ -35,23 +36,25 @@
             public static WaveType CharType { get; private set; }
             public static WaveType BoolType { get; private set; }
             public static WaveType ArrayType { get; private set; }
+            public static WaveType ExceptionType { get; private set; }
 
             internal static void Init()
             {
                 var asmName = "corlib%";
-                ObjectType  = new WaveTypeImpl($"{asmName}global::wave/lang/Object"   , WaveTypeCode.TYPE_OBJECT);
-                ValueType   = new WaveTypeImpl($"{asmName}global::wave/lang/ValueType", WaveTypeCode.TYPE_CLASS);
-                VoidType    = new WaveTypeImpl($"{asmName}global::wave/lang/Void"     , WaveTypeCode.TYPE_VOID);
-                StringType  = new WaveTypeImpl($"{asmName}global::wave/lang/String"   , WaveTypeCode.TYPE_STRING);
-                Int16Type   = new WaveTypeImpl($"{asmName}global::wave/lang/Int16"    , WaveTypeCode.TYPE_I2);
-                Int32Type   = new WaveTypeImpl($"{asmName}global::wave/lang/Int32"    , WaveTypeCode.TYPE_I4);
-                Int64Type   = new WaveTypeImpl($"{asmName}global::wave/lang/Int64"    , WaveTypeCode.TYPE_I8);
-                FloatType   = new WaveTypeImpl($"{asmName}global::wave/lang/Float"    , WaveTypeCode.TYPE_R4);
-                DoubleType  = new WaveTypeImpl($"{asmName}global::wave/lang/Double"   , WaveTypeCode.TYPE_R8);
-                DecimalType = new WaveTypeImpl($"{asmName}global::wave/lang/Decimal"  , WaveTypeCode.TYPE_R16);
-                CharType    = new WaveTypeImpl($"{asmName}global::wave/lang/Char"     , WaveTypeCode.TYPE_CHAR);
-                BoolType    = new WaveTypeImpl($"{asmName}global::wave/lang/Boolean"  , WaveTypeCode.TYPE_BOOLEAN);
-                ArrayType   = new WaveTypeImpl($"{asmName}global::wave/lang/Array"    , WaveTypeCode.TYPE_ARRAY);
+                ObjectType      = new WaveTypeImpl($"{asmName}global::wave/lang/Object"   , WaveTypeCode.TYPE_OBJECT);
+                ValueType       = new WaveTypeImpl($"{asmName}global::wave/lang/ValueType", WaveTypeCode.TYPE_CLASS);
+                VoidType        = new WaveTypeImpl($"{asmName}global::wave/lang/Void"     , WaveTypeCode.TYPE_VOID);
+                StringType      = new WaveTypeImpl($"{asmName}global::wave/lang/String"   , WaveTypeCode.TYPE_STRING);
+                Int16Type       = new WaveTypeImpl($"{asmName}global::wave/lang/Int16"    , WaveTypeCode.TYPE_I2);
+                Int32Type       = new WaveTypeImpl($"{asmName}global::wave/lang/Int32"    , WaveTypeCode.TYPE_I4);
+                Int64Type       = new WaveTypeImpl($"{asmName}global::wave/lang/Int64"    , WaveTypeCode.TYPE_I8);
+                FloatType       = new WaveTypeImpl($"{asmName}global::wave/lang/Float"    , WaveTypeCode.TYPE_R4);
+                DoubleType      = new WaveTypeImpl($"{asmName}global::wave/lang/Double"   , WaveTypeCode.TYPE_R8);
+                DecimalType     = new WaveTypeImpl($"{asmName}global::wave/lang/Decimal"  , WaveTypeCode.TYPE_R16);
+                CharType        = new WaveTypeImpl($"{asmName}global::wave/lang/Char"     , WaveTypeCode.TYPE_CHAR);
+                BoolType        = new WaveTypeImpl($"{asmName}global::wave/lang/Boolean"  , WaveTypeCode.TYPE_BOOLEAN);
+                ArrayType       = new WaveTypeImpl($"{asmName}global::wave/lang/Array"    , WaveTypeCode.TYPE_ARRAY);
+                ExceptionType   = new WaveTypeImpl($"{asmName}global::wave/lang/Exception", WaveTypeCode.TYPE_CLASS);
             }
         }
         public static WaveClass ObjectClass;
@@ -64,6 +67,7 @@
         public static WaveClass BoolClass;
         public static WaveClass CharClass;
         public static WaveClass ArrayClass;
+        public static WaveClass ExceptionClass;
 
         public static List<WaveClass> All => new()
         {
@@ -76,7 +80,8 @@
             Int16Class,
             BoolClass,
             CharClass,
-            ArrayClass
+            ArrayClass,
+            ExceptionClass
         };
         
         public static void Init()
@@ -101,6 +106,7 @@
             ArrayClass.DefineMethod("resize", Types.ArrayType,
                 MethodFlags.Public | MethodFlags.Extern | MethodFlags.Static,
                 ("arr", WaveTypeCode.TYPE_ARRAY), ("newSize", WaveTypeCode.TYPE_I4));
+            ExceptionClass.DefineMethod("ctor", WaveTypeCode.TYPE_VOID.AsType(), MethodFlags.Public);
         }
         
         static WaveCore()
