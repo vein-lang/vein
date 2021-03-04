a515710f2feeca2611481ee82674f77fec28b272
Yuuki Wesp <ls-micro@ya.ru>
2021-03-05 02:49:10 +0300

    bump spells

---
+++ spell.sh
@@ -0,0 +1,12 @@
+pushd $(git rev-parse --show-toplevel)
+perl -e '
+my $new_expect_file=".github/actions/spelling/expect.txt";
+use File::Path qw(make_path);
+make_path ".github/actions/spelling";
+open FILE, q{<}, $new_expect_file; chomp(my @words = <FILE>); close FILE;
+my @add=qw('"Bwave ECore Edot EFilter EModel EPipeline ESolution EString Fterm Fullset twodot wcorlib "');
+my %items; @items{@words} = @words x (1); @items{@add} = @add x (1);
+@words = sort {lc($a) cmp lc($b)} keys %items;
+open FILE, q{>}, $new_expect_file; for my $word (@words) { print FILE "$word\n" if $word =~ /\w/; };
+close FILE;'
+popd
