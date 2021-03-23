a00fd45f6d2c2d7ef952e8572fced6d10dfa5e82
Yuuki Wesp <ls-micro@ya.ru>
2021-03-23 05:55:45 +0300

    temporary fix arg expression

--- compiler/syntax/ast/expressions/ArgumentExpression.cs
+++ compiler/syntax/ast/expressions/ArgumentExpression.cs
@@ -10,8 +10,8 @@
 
         public ArgumentExpression(IOption<IdentifierExpression> id, IOption<ExpressionSyntax> t, ExpressionSyntax v)
         {
-            this.Identifier = id.GetOrDefault();
-            this.Type = t.GetOrDefault();
+            this.Identifier = id?.GetOrDefault();
+            this.Type = t?.GetOrDefault();
             this.Value = v;
         }
         public new ArgumentExpression SetPos(Position startPos, int length)
