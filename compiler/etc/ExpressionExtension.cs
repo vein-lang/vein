5f0ab3b880c75393a3f982380b473d56b8b17e33
Yuuki Wesp <ls-micro@ya.ru>
2021-04-05 03:52:34 +0300

    add optimization flag

--- compiler/etc/ExpressionExtension.cs
+++ compiler/etc/ExpressionExtension.cs
@@ -2,11 +2,14 @@
 {
     using System;
     using insomnia.syntax;
+    using Sprache;
 
     internal static class ExpressionExtension
     {
         public static bool CanOptimization(this ExpressionSyntax exp)
         {
+            if (exp.HasOptimized)
+                return false;
             if (exp is ArgumentExpression arg)
                 return true;
             if (exp is StringLiteralExpressionSyntax)
@@ -29,13 +32,13 @@
             if (exp is ArgumentExpression arg)
                 return ForceOptimization(arg.Value);
             if (exp is StringLiteralExpressionSyntax)
-                return exp;
+                return exp.AsOptimized();
 
             var result = new Expressive.Expression(exp.ExpressionString).Evaluate();
 
             if (result is float f)
-                return new SingleLiteralExpressionSyntax(f);
-            return new UndefinedIntegerNumericLiteral(result.ToString());
+                return new SingleLiteralExpressionSyntax(f).AsOptimized();
+            return new WaveSyntax().LiteralExpression.End().Parse($"{result}").AsOptimized();
         }
     }
 }
