6aead54b7ec0d6a0f9b456962243659d69b3ad20
Yuuki Wesp <ls-micro@ya.ru>
2021-04-19 02:02:48 +0300

    add ReadTypeName, ReadFieldName, reading types and fields table

--- ishtar/include/emit/module_reader.hpp
+++ ishtar/include/emit/module_reader.hpp
@@ -19,12 +19,31 @@ struct DecodedIL
     ILWrapper* il;
     dictionary<int, ILLabel>* map = new dictionary<int, ILLabel>();
 };
+
+auto ReadTypeName(WaveModule* mod, BinaryReader* reader)
+{
+    auto idx = reader->Read4();
+    auto result = mod->types_table->GetOrDefault(idx);
+    if (result == nullptr)
+        throw L"TypeName by index '{typeIndex}' not found in '{module.Name}' module."; // TODO
+    return result;
+}
+
+auto ReadFieldName(WaveModule* mod, BinaryReader* reader)
+{
+    auto idx = reader->Read4();
+    auto result = mod->fields_table->GetOrDefault(idx);
+    if (result == nullptr)
+        throw L"TypeName by index '{typeIndex}' not found in '{module.Name}' module."; // TODO
+    return result;
+}
+
 auto decodeLabels(BinaryReader* reader, int offset)
 {
     reader->Seek(offset);
     const auto labels_size = reader->Read4();
     if (labels_size == 0)
-       return new list_t<int>();
+        return new list_t<int>();
     auto result = new list_t<int>();
     for (auto i = 0; i != labels_size; i++)
     {
@@ -39,7 +58,7 @@ auto decodeIL(BinaryReader* reader, int* offset) noexcept(false)
     auto* const il_result = new DecodedIL();
     il_result->il = new ILWrapper();
     auto* result = new list_t<uint32_t>();
-    
+
     while (reader->Position() < reader->Length())
     {
         auto opcode = reader->Read2();
@@ -63,68 +82,68 @@ auto decodeIL(BinaryReader* reader, int* offset) noexcept(false)
             continue;
 
         if (opcode >= (uint16_t)LAST)
-            throw CorruptILException(fmt::format(L"OpCode '{0}' is not found in metadata.\n{1}", 
-                                                 opcode, L"re-run 'gen.csx' for fix this error."));
+            throw CorruptILException(fmt::format(L"OpCode '{0}' is not found in metadata.\n{1}",
+                opcode, L"re-run 'gen.csx' for fix this error."));
 
         const auto* opcode_name = opcodes[opcode];
         auto size = static_cast<int>(opcode_size[opcode]);
         printf("\t\t\t\t\t[ .%s, %d, %d]\n", opcode_name, opcode, size);
-        
-        auto label = ILLabel {
+
+        auto label = ILLabel{
             static_cast<WaveOpCode>(opcode),
             static_cast<int>(result->size())
         };
         auto ke = static_cast<int>(reader->Position() - sizeof(int16_t));
-        il_result->map->insert({ke, label});
+        il_result->map->insert({ ke, label });
 
         result->push_back(opcode);
-        
+
         switch (size)
         {
-            case 0:
-                continue;
-            case sizeof(char):
+        case 0:
+            continue;
+            case sizeof(char) :
                 result->push_back(reader->ReadByte());
                 continue;
-            case sizeof(int16_t):
-                result->push_back(reader->Read2());
-                continue;
-            case sizeof(int32_t):
-                {
-                    auto s = reader->Read4();
-                    auto d = static_cast<uint32_t>(s);
-                    result->push_back(d);
-                }
-                continue;
-            case sizeof(int64_t):
-                result->push_back(reader->Read4());
-                result->push_back(reader->Read4());
-                continue;
-            case 16:
-                result->push_back(reader->Read4());
-                result->push_back(reader->Read4());
-                result->push_back(reader->Read4());
-                result->push_back(reader->Read4());
-                continue;
-            default:
-                switch (opcode)
-                {
-                    case CALL:
-                        result->push_back(reader->ReadByte());
-                        result->push_back(reader->Read4());
-                        result->push_back(reader->Read4());
-                        result->push_back(reader->Read4());
-                        result->push_back(reader->Read4());
+                case sizeof(int16_t) :
+                    result->push_back(reader->Read2());
                     continue;
-                    case LOC_INIT_X:
-                        result->push_back(reader->Read4());
-                        result->push_back(reader->Read4());
-                        result->push_back(reader->Read4());
+                    case sizeof(int32_t) :
+                    {
+                        auto s = reader->Read4();
+                        auto d = static_cast<uint32_t>(s);
+                        result->push_back(d);
+                    }
                     continue;
-                }
-                
-                throw CorruptILException(fmt::format(L"OpCode '{0}' has invalid size [{1}].\n{2}", opcode, size,
-                        L"Check 'opcodes.def' and re-run 'gen.csx' for fix this error."));
+                    case sizeof(int64_t) :
+                        result->push_back(reader->Read4());
+                        result->push_back(reader->Read4());
+                        continue;
+                    case 16:
+                        result->push_back(reader->Read4());
+                        result->push_back(reader->Read4());
+                        result->push_back(reader->Read4());
+                        result->push_back(reader->Read4());
+                        continue;
+                    default:
+                        switch (opcode)
+                        {
+                        case CALL:
+                            result->push_back(reader->ReadByte());
+                            result->push_back(reader->Read4());
+                            result->push_back(reader->Read4());
+                            result->push_back(reader->Read4());
+                            result->push_back(reader->Read4());
+                            continue;
+                        case LOC_INIT_X:
+                            result->push_back(reader->Read4());
+                            result->push_back(reader->Read4());
+                            result->push_back(reader->Read4());
+                            continue;
+                        }
+
+                        throw CorruptILException(fmt::format(L"OpCode '{0}' has invalid size [{1}].\n{2}", opcode, size,
+                            L"Check 'opcodes.def' and re-run 'gen.csx' for fix this error."));
         }
     }
     il_result->il->code = new uint32_t[result->size()];
@@ -140,15 +159,11 @@ auto* readArguments(BinaryReader* reader, WaveModule* m) noexcept
 {
     auto* args = new list_t<WaveArgumentRef*>();
     auto size = reader->Read4();
-
-    function<wstring(int z)> wn = [m](int w) {
-        return m->GetConstByIndex(w);
-    };
-
+    
     for (auto i = 0; i != size; i++)
     {
         const auto nIdx = reader->Read4();
-        auto* tName = reader->ReadTypeName(wn);
+        auto* tName = ReadTypeName(m, reader);
         auto* result = new WaveArgumentRef();
 
         result->Type = m->FindType(tName, true);
@@ -161,16 +176,13 @@ auto* readArguments(BinaryReader* reader, WaveModule* m) noexcept
 [[nodiscard]]
 WaveMethod* readMethod(BinaryReader* reader, WaveClass* clazz, WaveModule* m) noexcept
 {
-    function<wstring(int z)> wn = [m](const int w) {
-        return m->GetConstByIndex(w);
-    };
 
     const auto idx = reader->Read4();
     const auto flags = static_cast<MethodFlags>(reader->ReadByte());
     const auto bodySize = reader->Read4();
     const auto stackSize = reader->ReadByte();
     const auto locals = reader->ReadByte();
-    const auto retTypeName = reader->ReadTypeName(wn);
+    const auto retTypeName = ReadTypeName(m, reader);
     auto* args = readArguments(reader, m);
     auto* body = reader->ReadBytes(bodySize);
 
@@ -225,12 +237,12 @@ WaveClass* readClass(BinaryReader* reader, WaveModule* m) noexcept(false)
         return m->GetConstByIndex(w);
     };
 
-    auto* const className = reader->ReadTypeName(wn);
+    auto* const className = ReadTypeName(m, reader);
     const auto flags = static_cast<ClassFlags>(reader->ReadByte());
 
-    auto* const parentName = reader->ReadTypeName(wn);
+    auto* const parentName = ReadTypeName(m, reader);
     const auto len = reader->Read4();
-        
+
     auto* const parent = m->FindType(parentName, true);
     auto* clazz = new WaveClass(className, AsClass(parent));
     clazz->Flags = flags;
@@ -251,11 +263,10 @@ WaveClass* readClass(BinaryReader* reader, WaveModule* m) noexcept(false)
     const auto fsize = reader->Read4();
     for (auto i = 0; i != fsize; i++)
     {
-        const auto fnameIdx = reader->Read8();
-        auto* fname = FieldName::construct(fnameIdx, &wn);
-        const auto tname = reader->ReadTypeName(wn);
+        auto* fname = ReadFieldName(m, reader);
+        const auto tname = ReadTypeName(m, reader);
         auto* const return_type = m->FindType(tname, true);
-        const auto fflags  = static_cast<FieldFlags>(reader->ReadByte());
+        const auto fflags = static_cast<FieldFlags>(reader->ReadByte());
         //auto litVal = reader->ReadBytes(0);
         auto* field = new WaveField(clazz, fname, fflags, return_type);
         clazz->Fields->push_back(field);
@@ -275,17 +286,62 @@ WaveModule* readModule(char* arr, size_t sz, list_t<WaveModule*>* deps) noexcept
     auto* reader = new BinaryReader(mem);
 
     target->deps = deps;
-    
-    const auto idx = reader->Read4();
+
+    const auto idx = reader->Read4(); // name
+    const auto vdx = reader->Read4(); // version
+
+
     const auto ssize = reader->Read4();
     printf("\t[construct const string table] size: %d\n", ssize);
     for (auto i = 0; i != ssize; i++)
     {
         auto key = reader->Read4();
         auto str = reader->ReadInsomniaString();
-        target->strings->insert({key, str});
+        target->strings_table->insert({ key, str });
         printf("\t\t{'%d'}: '%ws'\n", key, str.c_str());
     }
+    // read types table
+
+    const int tsize = reader->Read4();
+    for (auto i = 0; i != tsize; i++)
+    {
+        auto key = reader->Read4();
+        auto asmName = reader->ReadInsomniaString();
+        auto ns = reader->ReadInsomniaString();
+        auto name = reader->ReadInsomniaString();
+        auto rs = new TypeName(asmName, name, ns);
+        target->types_table->insert({key, rs});
+    }
+    // read fields table
+    const int fsize = reader->Read4();
+    for (auto i = 0; i != fsize; i++)
+    {
+        auto key = reader->Read4();
+        auto name = reader->ReadInsomniaString();
+        auto clazz = reader->ReadInsomniaString();
+        auto rs = new FieldName(name, clazz);
+        target->fields_table->insert({key, rs});
+    }
+    // read deps
+    const int dsize = reader->Read4();
+    for (auto i = 0; i != dsize; i++)
+    {
+        auto name = reader->ReadInsomniaString();
+        auto version = reader->ReadInsomniaString();
+
+        printf("\tDEP: %ws,%ws\n", name.c_str(), version.c_str());
+    }
+    /*// read deps refs
+            foreach (var _ in ..reader.ReadInt32())
+            {
+                var name = reader.ReadInsomniaString();
+                var ver = Version.Parse(reader.ReadInsomniaString());
+                if (module.Deps.Any(x => x.Version.Equals(ver) && x.Name.Equals(name))) 
+                    continue;
+                var dep = resolver(name, ver);
+                module.Deps.Add(dep);
+            }*/
+
     const auto csize = reader->Read4();
     printf("\t[construct class table] size: %d\n", csize);
     for (auto i = 0; i != csize; i++)
@@ -297,14 +353,22 @@ WaveModule* readModule(char* arr, size_t sz, list_t<WaveModule*>* deps) noexcept
         printf("\t\t{\n");
         auto* clazz = readClass(classReader, target);
         printf("\t\t}\n;");
-        target->classList->push_back(clazz);
+        target->class_table->push_back(clazz);
         //delete classReader;
         //delete classMem;
         delete body;
     }
     target->name = target->GetConstByIndex(idx);
+    target->VERSION = target->GetRefConstByIndex(vdx);
+
+
+    // read const storage
+    const auto storage_size = reader->Read4();
+    const auto unused = reader->ReadBytes(storage_size);
+    delete unused;
+
     printf("\tload '%ws' module success\n", target->name.c_str());
 
-    
+
     return target;
 }
