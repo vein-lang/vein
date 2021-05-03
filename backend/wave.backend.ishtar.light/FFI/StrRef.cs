1e8aa420af762262258d21e321c6c32a8949cd04
Yuuki Wesp <ls-micro@ya.ru>
2021-05-03 03:52:49 +0300

    add interning strings

---
+++ backend/wave.backend.ishtar.light/FFI/StrRef.cs
@@ -0,0 +1,9 @@
+﻿namespace ishtar
+{
+    public unsafe struct StrRef
+    {
+        public static string Unwrap(StrRef* p) => StringStorage.GetString(p);
+
+        public ulong index;
+    }
+}
---
+++ backend/wave.backend.ishtar.light/FFI/StringStorage.cs
@@ -0,0 +1,42 @@
+﻿namespace ishtar
+{
+    using System.Collections.Generic;
+    using System.Runtime.InteropServices;
+
+    public static unsafe class StringStorage
+    {
+        private static readonly Dictionary<string, ulong> storage_r = new();
+        private static readonly Dictionary<ulong, string> storage_l = new();
+
+        public static StrRef* Intern(string value)
+        {
+            if (storage_r.ContainsKey(value))
+                return (StrRef*)storage_r[value];
+
+            var p = (StrRef*) Marshal.AllocHGlobal(sizeof(StrRef));
+            
+            if (p == null)
+            {
+                VM.FastFail(WNE.OUT_OF_MEMORY, "Cannot allocate string reference.");
+                VM.ValidateLastError();
+                return null;
+            }
+            p->index = (ulong)storage_r.Count + 1;
+            storage_r.Add(value, p->index);
+            storage_l.Add(p->index, value);
+            return p;
+        }
+
+        public static string GetString(StrRef* p)
+        {
+            FFI.StaticValidate(p);
+            if (!storage_l.ContainsKey(p->index))
+            {
+                VM.FastFail(WNE.ACCESS_VIOLATION, "Pointer incorrect.");
+                VM.ValidateLastError();
+                return null;
+            }
+            return storage_l[p->index];
+        }
+    }
+}
