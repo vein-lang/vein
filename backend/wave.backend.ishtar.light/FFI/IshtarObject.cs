fb44f7619abafc3b9ce7c01ea21ab4aa49d4a1d5
Yuuki Wesp <ls-micro@ya.ru>
2021-05-03 03:53:24 +0300

    add ishtar object

---
+++ backend/wave.backend.ishtar.light/FFI/IshtarObject.cs
@@ -0,0 +1,22 @@
+﻿namespace ishtar
+{
+    using System.Runtime.CompilerServices;
+
+    public unsafe struct IshtarObject
+    {
+        public void* clazz;
+        public void** vtable;
+        public GCFlags flags;
+
+        public uint vtable_size;
+
+        public IshtarObject** owner;
+
+        public RuntimeIshtarClass Unpack()
+        {
+            if (clazz is null)
+                return null;
+            return IshtarUnsafe.AsRef<RuntimeIshtarClass>(clazz);
+        }
+    }
+}
