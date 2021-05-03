ce364e9626cee612df0142c08d7077bc193f34b2
Yuuki Wesp <ls-micro@ya.ru>
2021-05-03 03:51:14 +0300

    add RuntimeIshtarField

---
+++ backend/wave.backend.ishtar.light/runtime/vm/RuntimeIshtarField.cs
@@ -0,0 +1,15 @@
+﻿namespace ishtar
+{
+    using wave.runtime;
+
+    public unsafe class RuntimeIshtarField : WaveField
+    {
+        public RuntimeIshtarField(WaveClass owner, FieldName fullName, FieldFlags flags, WaveClass fieldType) : 
+            base(owner, fullName, flags, fieldType)
+        { }
+
+
+        public int vtable_offset = 0;
+        public void* default_value;
+    }
+}
