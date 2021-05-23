597ec8ec908cdaebfb657be1c8d11438bdad122b
Yuuki Wesp <ls-micro@ya.ru>
2021-05-23 22:30:05 +0300

    rework local variable declaration and set it obsolete

--- lib/ast/syntax/Declarations.cs
+++ lib/ast/syntax/Declarations.cs
@@ -1,34 +1,33 @@
 ﻿namespace mana.syntax
 {
+    using System;
     using Sprache;
     using stl;
-
+    
     public partial class ManaSyntax
     {
-        // example: now = DateTime.Now()
-        protected internal virtual Parser<VariableDeclaratorSyntax> VariableDeclarator =>
-           (from identifier in Identifier
-               from exp in Parse.Char('=').Token().Then(_ => QualifiedExpression).Positioned().Optional()
-               select new VariableDeclaratorSyntax
-               {
-                   Identifier = identifier,
-                   Expression = ExpressionSyntax.CreateOrDefault(exp),
-               }).Positioned();
+        private Parser<ExpressionSyntax> _assignExpression =>
+            from op in Parse.Char('=').Token()
+            from exp in QualifiedExpression
+            select exp;
         // auto i: int = 3;
+        [Obsolete]
         protected internal virtual Parser<VariableDeclarationSyntax> VariableDeclaration =>
-            (from keyword in Keyword("auto").Token()
-                from declarator in VariableDeclarator.Commented(this)
+            (from keyword in KeywordExpression("auto").Token()
+                from declarator in IdentifierExpression.Token().Commented(this)
                 from @as in Parse.Char(':').Token().Commented(this)
-                from type in TypeReference.Token().Positioned().Commented(this)
-                from semicolon in Parse.Char(';')
+                from type in TypeExpression.Token().Positioned().Commented(this)
+                from exp in _assignExpression.Token().Optional()
+                from semicolon in Parse.Char(';').Token()
                 select new VariableDeclarationSyntax
                 {
                     Type = type.Value,
-                    Variables = declarator.Value,
+                    Variable = declarator.Value,
+                    ExpressionValue = exp.GetOrDefault()
                 }
-                    .WithLeadingComments(declarator.LeadingComments).WithTrailingComments(declarator.TrailingComments)
-                    .WithLeadingComments(@as.LeadingComments).WithTrailingComments(@as.TrailingComments)
-                    .WithLeadingComments(type.LeadingComments).WithTrailingComments(type.TrailingComments)
+                .WithLeadingComments(declarator.LeadingComments).WithTrailingComments(declarator.TrailingComments)
+                .WithLeadingComments(@as.LeadingComments).WithTrailingComments(@as.TrailingComments)
+                .WithLeadingComments(type.LeadingComments).WithTrailingComments(type.TrailingComments)
             ).Positioned();
     }
 }
