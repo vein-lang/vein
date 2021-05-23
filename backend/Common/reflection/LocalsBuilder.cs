168746e4d8c2244989a2557898a6c617c38a4067
Yuuki Wesp <ls-micro@ya.ru>
2021-05-23 22:21:55 +0300

    add into LocalsBuilder ToString method, add Mark fn for debug info

--- backend/Common/reflection/LocalsBuilder.cs
+++ backend/Common/reflection/LocalsBuilder.cs
@@ -2,12 +2,16 @@
 {
     using System.Collections;
     using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
     /// <summary>
     /// Locals builder.
     /// </summary>
     public class LocalsBuilder : IEnumerable<QualityTypeName>
     {
         private readonly IList<QualityTypeName> types = new List<QualityTypeName>();
+        private readonly Dictionary<int, string> locals_dictionary = new();
         public void Push(QualityTypeName type) 
             => types.Add(type);
         public void Push(ManaType type) 
@@ -17,6 +21,7 @@
         public void Push(ManaTypeCode type)
             => types.Add(type.AsClass().FullName);
 
+        public void Mark(int index, string variable) => locals_dictionary[index] = variable;
 
         #region Implementation of IEnumerable
 
@@ -56,6 +61,21 @@
             foreach (var type in arr) 
                 l.Push(type);
             return l;
+        }
+
+        
+        public override string ToString()
+        {
+            if (types.Count == 0)
+                return ".locals { }";
+            var str = new StringBuilder();
+            str.AppendLine(".locals { ");
+            foreach (var (t, i) in this.Select((x, y) => (x, y)))
+                str.AppendLine(locals_dictionary.ContainsKey(i)
+                    ? $"\t[{i}]: {t.Name}, {{'{locals_dictionary[i]}'}}"
+                    : $"\t[{i}]: {t.Name}");
+            str.Append("};");
+            return str.ToString();
         }
     }
 }
