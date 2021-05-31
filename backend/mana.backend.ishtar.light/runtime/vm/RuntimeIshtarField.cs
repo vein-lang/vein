b5f4f4baa419de139a7b5ff043267e34907c64da
Yuuki Wesp <ls-micro@ya.ru>
2021-05-31 04:23:00 +0300

    add method for detect necessity remapping #23

--- backend/mana.backend.ishtar.light/runtime/vm/RuntimeIshtarField.cs
+++ backend/mana.backend.ishtar.light/runtime/vm/RuntimeIshtarField.cs
@@ -1,5 +1,6 @@
 ﻿namespace ishtar
 {
+    using System.Linq;
     using mana.runtime;
 
     public unsafe class RuntimeIshtarField : ManaField
@@ -11,5 +12,43 @@
 
         public int vtable_offset = 0;
         public void* default_value;
+
+
+        public bool IsNeedReMapping(out int new_offset)
+        {
+            bool failMapping(int code)
+            {
+                VM.FastFail(WNE.TYPE_LOAD, 
+                    $"Native aspect has incorrect mapping for '{FullName}' field. [0x{code:X}]");
+                VM.ValidateLastError();
+                return false;
+            }
+
+            new_offset = 0;
+            var nativeAspect = Aspects.FirstOrDefault(x => x.Name == "Native");
+            if (nativeAspect is null)
+                return false;
+            if (nativeAspect.Arguments.Count != 1)
+                return failMapping(0);
+            var arg = nativeAspect.Arguments.First().Value;
+
+            if (arg is not string existName)
+                return failMapping(1);
+
+            var existField = Owner.Fields.FirstOrDefault(x => x.Name.Equals(existName));
+
+            if (existField is null)
+                return failMapping(2);
+
+            if (existField.FieldType != FieldType)
+                return failMapping(3);
+
+            if (existField is not RuntimeIshtarField runtimeField)
+                return failMapping(4);
+
+            new_offset = runtimeField.vtable_offset;
+
+            return true;
+        }
     }
 }
