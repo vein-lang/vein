d25379ca42a261cba211e582c6aff64639d9e204
Yuuki Wesp <ls-micro@ya.ru>
2021-05-31 03:48:51 +0300

    rework const storage reading\writing #12

--- backend/Ishtar/emit/extensions/ConstStorageExtensions.cs
+++ backend/Ishtar/emit/extensions/ConstStorageExtensions.cs
@@ -2,7 +2,11 @@
 {
     using System;
     using System.IO;
+    using System.Linq;
+    using System.Reflection;
     using System.Text;
+    using exceptions;
+    using mana.extensions;
     using mana.runtime;
 
     public static class ConstStorageExtensions
@@ -11,65 +15,42 @@
         {
             using var mem = new MemoryStream();
             using var bin = new BinaryWriter(mem);
+            bin.Write(cs.storage.Count);
             foreach (var (key, value) in cs.storage)
             {
-                switch (Type.GetTypeCode(value.GetType()))
+                try
                 {
-                    case TypeCode.Object when value is Half hf:
-                        bin.Write((float)hf);
-                        break;
-                    case TypeCode.Empty:
-                    case TypeCode.Object:
-                    case TypeCode.DateTime:
-                    case TypeCode.Char:
-                    case TypeCode.DBNull:
-                        throw new ConstCannotUseNonPrimitiveTypeException(key, value.GetType());
-                    case TypeCode.Boolean:
-                        bin.Write((bool)value);
-                        break;
-                    case TypeCode.SByte:
-                        bin.Write((sbyte)value);
-                        break;
-                    case TypeCode.Byte:
-                        bin.Write((byte)value);
-                        break;
-                    case TypeCode.Int16:
-                        bin.Write((short)value);
-                        break;
-                    case TypeCode.UInt16:
-                        bin.Write((ushort)value);
-                        break;
-                    case TypeCode.Int32:
-                        bin.Write((int)value);
-                        break;
-                    case TypeCode.UInt32:
-                        bin.Write((uint)value);
-                        break;
-                    case TypeCode.Int64:
-                        bin.Write((long)value);
-                        break;
-                    case TypeCode.UInt64:
-                        bin.Write((ulong)value);
-                        break;
-                    case TypeCode.Single:
-                        bin.Write((float)value);
-                        break;
-                    case TypeCode.Double:
-                        bin.Write((double)value);
-                        break;
-                    case TypeCode.Decimal:
-                        bin.Write((decimal)value);
-                        break;
-                    case TypeCode.String:
-                        bin.WriteInsomniaString((string)value);
-                        break;
+                    var type_code = value.DetermineTypeCode();
+
+                    bin.Write((int)type_code);
+                    bin.WriteInsomniaString(key.fullName);
+                    bin.WriteInsomniaString(value.ToString());
                 }
+                catch (NotSupportedException e)
+                {
+                    throw new ObjectIsNotValueType($"{e.Message}, '{value}', {value.GetType()}, '{key}'");
+                }
+               
             }
 
-            return mem.ToArray();
+            return mem.ToArray(); // (int)Convert.ChangeType("12", typeof(int))
         }
 
+        public static ConstStorage ToConstStorage(this byte[] arr)
+        {
+            using var mem = new MemoryStream(arr);
+            using var bin = new BinaryReader(mem);
+            var storage = new ConstStorage();
+            foreach (var i in ..bin.ReadInt32())
+            {
+                var type_code = (ManaTypeCode)bin.ReadInt32();
+                var fullname = bin.ReadInsomniaString();
+                var value = bin.ReadInsomniaString();
+                storage.Stage(new FieldName(fullname), Convert.ChangeType(value, type_code.ToCLRTypeCode()));
+            }
 
+            return storage;
+        }
         public static string BakeDebugString(this ConstStorage cs)
         {
             var str = new StringBuilder();
