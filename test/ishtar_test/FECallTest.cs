ff383ade0e782a89e1cec4b69e4a8acd58b73721
Yuuki Wesp <ls-micro@ya.ru>
2021-05-31 06:18:39 +0300

    add additional FFI call test

---
+++ test/ishtar_test/FECallTest.cs
@@ -0,0 +1,27 @@
+﻿namespace ishtar_test
+{
+    using ishtar;
+    using Xunit;
+
+    public class FECallTest : IshtarTestBase
+    {
+        [Fact]
+        public void Call_FE_Console_Println()
+        {
+            using var ctx = CreateContext();
+            ctx.OnClassBuild((x, storage) =>
+            {
+                var type = x.Owner.FindType("corlib%global::mana/lang/Out", true);
+
+                storage.method = type.FindMethod("@_println") as RuntimeIshtarMethod;
+            });
+
+            ctx.Execute((gen, storage) =>
+            {
+                gen.Emit(OpCodes.LDC_STR, "foo");
+                gen.Emit(OpCodes.CALL, (RuntimeIshtarMethod) storage.method);
+                gen.Emit(OpCodes.RET);
+            });
+        }
+    }
+}
