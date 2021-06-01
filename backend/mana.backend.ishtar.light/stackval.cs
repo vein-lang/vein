0738a0b60aff514a8836295233c3452560e67a9c
Yuuki Wesp <ls-micro@ya.ru>
2021-06-01 17:35:38 +0300

    vm members to out files

---
+++ backend/mana.backend.ishtar.light/stackval.cs
@@ -0,0 +1,13 @@
+﻿namespace ishtar
+{
+    using System;
+    using mana.runtime;
+
+    public struct stackval
+    {
+        public stack_union data;
+        public ManaTypeCode type;
+
+        public static stackval[] Alloc(int size) => GC.AllocateArray<stackval>(size, true);
+    }
+}
--- backend/mana.backend.ishtar.light/vm.cs
+++ backend/mana.backend.ishtar.light/vm.cs
@@ -671,29 +671,4 @@ namespace ishtar
             ValidateLastError();
         }
     }
-    
-    public struct stackval
-    {
-        public stack_union data;
-        public ManaTypeCode type;
-
-        public static stackval[] Alloc(int size) => GC.AllocateArray<stackval>(size, true);
-    }
-    [StructLayout(LayoutKind.Explicit)] 
-    public unsafe struct stack_union
-    {
-        [FieldOffset(0)] public sbyte b;
-        [FieldOffset(0)] public short s;
-        [FieldOffset(0)] public int i;
-        [FieldOffset(0)] public long l;
-        [FieldOffset(0)] public byte ub;
-        [FieldOffset(0)] public ushort us;
-        [FieldOffset(0)] public uint ui;
-        [FieldOffset(0)] public ulong ul;
-        [FieldOffset(0)] public float f_r4;
-        [FieldOffset(0)] public double f;
-        [FieldOffset(0)] public decimal d;
-        [FieldOffset(0)] public Half hf;
-        [FieldOffset(0)] public IntPtr p;
-    }
 }
