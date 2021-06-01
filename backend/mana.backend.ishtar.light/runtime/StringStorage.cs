068861774f456cdb3c67886a98ae1aa65c7660ce
Yuuki Wesp <ls-micro@ya.ru>
2021-06-01 17:36:32 +0300

    drop using StrRef with index, drop allocation global block of memory

--- backend/mana.backend.ishtar.light/runtime/StringStorage.cs
+++ backend/mana.backend.ishtar.light/runtime/StringStorage.cs
@@ -6,40 +6,29 @@
 
     public static unsafe class StringStorage
     {
-        private static readonly Dictionary<string, ulong> storage_r = new();
-        private static readonly Dictionary<ulong, string> storage_l = new();
+        internal static readonly Dictionary<string, ulong> storage_r = new();
+        internal static readonly Dictionary<ulong, string> storage_l = new();
 
         public static StrRef* Intern(string value)
         {
             if (storage_r.ContainsKey(value))
                 return (StrRef*)storage_r[value];
-
-            var p = (StrRef*) Marshal.AllocHGlobal(sizeof(StrRef));
-            
-            Unsafe.InitBlock(p, 0, (uint)sizeof(StrRef));
-            
-            if (p == null)
-            {
-                VM.FastFail(WNE.OUT_OF_MEMORY, "Cannot allocate string reference.");
-                VM.ValidateLastError();
-                return null;
-            }
-            p->index = (ulong)storage_r.Count + 1;
-            storage_r.Add(value, p->index);
-            storage_l.Add(p->index, value);
-            return p;
+            var p = (ulong)storage_r.Count + 1;
+            storage_r.Add(value, p);
+            storage_l.Add(p, value);
+            return (StrRef*)p;
         }
 
         public static string GetString(StrRef* p)
         {
             FFI.StaticValidate(p);
-            if (!storage_l.ContainsKey(p->index))
+            if (!storage_l.ContainsKey((ulong)p))
             {
                 VM.FastFail(WNE.ACCESS_VIOLATION, "Pointer incorrect.");
                 VM.ValidateLastError();
                 return null;
             }
-            return storage_l[p->index];
+            return storage_l[(ulong)p];
         }
     }
 }
