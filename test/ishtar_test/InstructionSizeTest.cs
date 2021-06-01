94929b465fc6281881c972b66196a26d43198183
Yuuki Wesp <ls-micro@ya.ru>
2021-06-01 17:38:25 +0300

    add I8 size instruction test

---
+++ test/ishtar_test/InstructionSizeTest.cs
@@ -0,0 +1,25 @@
+﻿namespace ishtar_test
+{
+    using ishtar;
+    using mana.ishtar.emit;
+    using mana.runtime;
+    using Xunit;
+
+    public unsafe class InstructionSizeTest : IshtarTestBase
+    {
+        [Fact]
+        public void I8FillTest()
+        {
+            using var ctx = CreateContext();
+
+            var result = ctx.Execute((gen, _) =>
+            {
+                gen.Emit(OpCodes.LDC_I8_S, long.MaxValue);
+                gen.Emit(OpCodes.RET);
+            });
+
+            Assert.Equal(ManaTypeCode.TYPE_I8, (*result.returnValue).type);
+            Assert.Equal(long.MaxValue, (*result.returnValue).data.l);
+        }
+    }
+}
