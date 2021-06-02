8b6bddd407ed6dbf2b855396279c8691289c8c48
Yuuki Wesp <ls-micro@ya.ru>
2021-06-02 18:05:47 +0300

    add array test

---
+++ test/wc_test/array_test.cs
@@ -0,0 +1,32 @@
+﻿namespace wc_test
+{
+    using mana.stl;
+    using mana.syntax;
+    using Xunit;
+    using Xunit.Abstractions;
+
+    public class array_test
+    {
+        private readonly ITestOutputHelper _debug;
+        public static ManaSyntax Syntax => new();
+
+        public array_test(ITestOutputHelper testOutputHelper) => _debug = testOutputHelper;
+
+
+        [Fact]
+        public void FirstArrayTest()
+        {
+            Syntax.ClassMemberDeclaration.ParseMana($"public x: arr[];");
+        }
+
+        [Fact(Skip = "Disabled")]
+        public void AccessArrayTest()
+        {
+            var result = Syntax
+                .Statement
+                .ParseMana($"{{x.x.x[1, 2, 3, \"\", variable, 8 * 8]}}");
+
+            Assert.False(result.IsBrokenToken);
+        }
+    }
+}
