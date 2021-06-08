2a7671bbeb12bf0d6661237748cfa97722ae2383
Yuuki Wesp <ls-micro@ya.ru>
2021-06-09 01:26:54 +0300

    add el_class ref, add set and get elements #27

--- backend/mana.backend.ishtar.light/runtime/IshtarArray.cs
+++ backend/mana.backend.ishtar.light/runtime/IshtarArray.cs
@@ -1,6 +1,7 @@
 namespace ishtar
 {
     using System;
+    using System.Runtime.CompilerServices;
 
     public unsafe struct IshtarArray : IEquatable<IshtarArray>
     {
@@ -16,12 +17,51 @@ namespace ishtar
             => (IshtarObject**)memory->vtable[_block.offset_value];
         public RuntimeIshtarClass Class
             => clazz is null ? null : IshtarUnsafe.AsRef<RuntimeIshtarClass>(clazz);
+        public RuntimeIshtarClass ElementClass
+            => element_clazz is null ? null : IshtarUnsafe.AsRef<RuntimeIshtarClass>(element_clazz);
 
         public void* clazz => memory->clazz;
+        public void* element_clazz;
 
         public IshtarObject* memory;
         public Block _block;
 
+        public IshtarObject* this[uint index, CallFrame? frame]
+        {
+            get => Get(index, frame);
+            set => Set(index, value, frame);
+        }
+
+        public IshtarObject* Get(uint index, CallFrame? frame = null)
+        {
+            if (!ElementClass.IsPrimitive) return elements[index];
+            var result = IshtarGC.AllocObject(ElementClass);
+            var offset = result->DecodeClass().Field["!!value"].vtable_offset;
+            result->vtable[offset] = elements[index]->vtable[offset];
+            return result;
+        }
+
+        public void Set(uint index, IshtarObject* value, CallFrame frame = null)
+        {
+            FFI.StaticValidate(frame, &value);
+            var value_class = value->DecodeClass();
+            VM.Assert(value_class.TypeCode == ElementClass.TypeCode, WNE.TYPE_MISMATCH, $"", frame);
+            if (index > length)
+            {
+                VM.FastFail(WNE.OUT_OF_RANGE, $"", frame);
+                VM.ValidateLastError();
+                return;
+            }
+            
+            if (Class.IsPrimitive)
+            {
+                var offset = value_class.Field["!!value"].vtable_offset;
+                elements[index]->vtable[offset] = value->vtable[offset];
+            }
+            else
+                elements[index] = value;
+        }
+        
 
         public struct Block : IEquatable<Block>
         {
