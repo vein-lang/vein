05ccf8568cbcaae16433650562d8142ad49bd9fa
Yuuki Wesp <ls-micro@ya.ru>
2021-06-08 13:06:02 +0300

    add ishtar array

---
+++ backend/mana.backend.ishtar.light/runtime/IshtarArray.cs
@@ -0,0 +1,21 @@
+﻿namespace ishtar
+{
+    using System;
+
+    public unsafe struct IshtarArray
+    {
+        public int rank;
+        public int len;
+        public IshtarObject** elements;
+
+        public void* clazz;
+
+
+        public RuntimeIshtarClass DecodeClass()
+        {
+            if (clazz is null)
+                return null;
+            return IshtarUnsafe.AsRef<RuntimeIshtarClass>(clazz);
+        }
+    }
+}
