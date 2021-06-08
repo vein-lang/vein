5e1cbb416ec9e68875a7af3129f61ee63aaa8be8
Yuuki Wesp <ls-micro@ya.ru>
2021-06-09 01:25:51 +0300

    add more test for #27

--- test/ishtar_test/GCTest.cs
+++ test/ishtar_test/GCTest.cs
@@ -1,11 +1,19 @@
 namespace ishtar_test
 {
+    using System;
     using ishtar;
+    using mana.extensions;
     using mana.runtime;
     using Xunit;
+    using Xunit.Abstractions;
 
     public class GCTest : IshtarTestBase
     {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public GCTest(ITestOutputHelper testOutputHelper)
+            => _testOutputHelper = testOutputHelper;
+
         [Fact]
         public unsafe void CorrectAllocateInt()
         {
@@ -37,10 +45,22 @@ namespace ishtar_test
         [Fact]
         public unsafe void CorrectAllocateArray()
         {
+            if (VM.watcher is DefaultWatchDog)
+                VM.watcher = new TestWatchDog();
+
             var array = IshtarGC.AllocArray(ManaTypeCode.TYPE_I4.AsRuntimeClass(), 10, 1);
 
             Assert.Equal(10UL, array->length);
             Assert.Equal(1UL, array->rank);
+
+            foreach (var i in ..10)
+                array->Set((uint) i, IshtarMarshal.ToIshtarObject(88 * i));
+
+            foreach (var i in ..10)
+            {
+                var obj = array->Get((uint) i);
+                Assert.Equal(i * 88, IshtarMarshal.ToDotnetInt32(obj, null));
+            }
         }
         protected override void StartUp() { }
         protected override void Shutdown() { }
