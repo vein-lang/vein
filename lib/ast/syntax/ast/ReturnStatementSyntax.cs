b5dd964e325b1d1cc40f03d255a33bcb19ac0eed
Yuuki Wesp <ls-micro@ya.ru>
2021-06-12 22:36:05 +0300

    fix missed SetPos for ReturnStatement

--- lib/ast/syntax/ast/ReturnStatementSyntax.cs
+++ lib/ast/syntax/ast/ReturnStatementSyntax.cs
@@ -1,13 +1,20 @@
 namespace mana.syntax
 {
     using System.Collections.Generic;
+    using Sprache;
 
-    public class ReturnStatementSyntax : StatementSyntax
+    public class ReturnStatementSyntax : StatementSyntax, IPositionAware<ReturnStatementSyntax>
     {
         public override SyntaxType Kind => SyntaxType.ReturnStatement;
 
         public override IEnumerable<BaseSyntax> ChildNodes => GetNodes(Expression);
 
         public ExpressionSyntax Expression { get; set; }
+
+        public new ReturnStatementSyntax SetPos(Position startPos, int length)
+        {
+            base.SetPos(startPos, length);
+            return this;
+        }
     }
 }
