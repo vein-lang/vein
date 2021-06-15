1c4660cb374e4baf2010d96ef36a91fa29dcf082
Yuuki Wesp <ls-micro@ya.ru>
2021-06-16 01:35:36 +0300

    add GetLen test #27

--- test/ishtar_test/ArrayTest.cs
+++ test/ishtar_test/ArrayTest.cs
@@ -50,5 +50,30 @@ namespace ishtar_test
             Assert.Equal(ManaTypeCode.TYPE_I4,result.returnValue->type);
             Assert.Equal(3, result.returnValue->data.i);
         }
+
+        [Fact]
+        public void GetLenTest()
+        {
+            using var ctx = CreateContext();
+            var arrType = ManaTypeCode.TYPE_I4.AsClass().FullName;
+
+            ctx.EnsureType(arrType);
+
+            var result = ctx.Execute((x, _) =>
+            {
+                x.Emit(OpCodes.LD_TYPE, arrType);
+                x.Emit(OpCodes.LDC_I8_5);
+                x.Emit(OpCodes.NEWARR);
+                x.Emit(OpCodes.LDC_I4_2);
+                x.Emit(OpCodes.STELEM_S, 0);
+                x.Emit(OpCodes.LDC_I4_3);
+                x.Emit(OpCodes.STELEM_S, 1);
+                x.Emit(OpCodes.LDLEN);
+                x.Emit(OpCodes.RET);
+            });
+
+            Assert.Equal(ManaTypeCode.TYPE_U8,result.returnValue->type);
+            Assert.Equal(5, result.returnValue->data.i);
+        }
     }
 }
