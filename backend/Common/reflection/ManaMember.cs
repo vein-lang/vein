e0bffa696161073c9da0844389c7e59071e856af
Yuuki Wesp <ls-micro@ya.ru>
2021-06-19 14:45:46 +0300

    split files

---
+++ backend/Common/reflection/ManaMember.cs
@@ -0,0 +1,9 @@
+namespace mana.runtime
+{
+    public abstract class ManaMember
+    {
+        public abstract string Name { get; protected set; }
+        public abstract ManaMemberKind Kind { get; }
+        public virtual bool IsSpecial { get; }
+    }
+}
---
+++ backend/Common/reflection/ManaMemberKind.cs
@@ -0,0 +1,16 @@
+namespace mana.runtime
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    [Flags]
+    public enum ManaMemberKind
+    {
+        Ctor    = 1 << 1,
+        Dtor    = 1 << 2,
+        Field   = 1 << 3,
+        Method  = 1 << 4,
+        Type    = 1 << 5
+    }
+}
