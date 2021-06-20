c2b03972b894a41cd31a8808787c8965a9ab067e
Yuuki Wesp <ls-micro@ya.ru>
2021-06-20 19:30:55 +0300

    clear unused code, change compile to build name of command

--- compiler/Program.cs
+++ compiler/Program.cs
@@ -1,21 +1,16 @@
 using System;
 using System.Diagnostics;
-using System.IO;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Globalization;
 using System.Runtime.CompilerServices;
-using System.Threading;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
-using Spectre.Console;
 using Spectre.Console.Cli;
 using insomnia;
 using mana.cmd;
 using static System.Console;
 using static GitVersionInformation;
 using static Spectre.Console.AnsiConsole;
-using Color = System.Drawing.Color;
 
 [assembly: InternalsVisibleTo("wc_test")]
 
@@ -25,10 +20,7 @@ if (Environment.GetEnvironmentVariable("WT_SESSION") == null && RuntimeInformati
     Environment.SetEnvironmentVariable($"RUNE_EMOJI_USE", "0");
     Environment.SetEnvironmentVariable($"RUNE_COLOR_USE", "0");
     Environment.SetEnvironmentVariable($"RUNE_NIER_USE", "0");
-    //Environment.SetEnvironmentVariable($"NO_COLOR", "true");
-    //ForegroundColor = ConsoleColor.Gray;
-    //Console.WriteLine($"\t@\tno windows-terminal: coloring, emoji and nier has disabled.");
-    //ForegroundColor = ConsoleColor.White;
+    Environment.SetEnvironmentVariable($"NO_COLOR", "true");
 }
 
 
@@ -66,7 +58,7 @@ var app = new CommandApp();
 
 app.Configure(config =>
 {
-    config.AddCommand<CompileCommand>("compile");
+    config.AddCommand<CompileCommand>("build");
 });
 
 var result = app.Run(args);
