70e25d42a86e090e5ca56c6a9eedd08e7f1d511b
Yuuki Wesp <ls-micro@ya.ru>
2021-06-25 00:09:25 +0300

    in packing pointer additional checks, suppress finalize #57

--- backend/mana.backend.ishtar.light/runtime/IshtarUnsafe.cs
+++ backend/mana.backend.ishtar.light/runtime/IshtarUnsafe.cs
@@ -1,19 +1,26 @@
 namespace ishtar
 {
+    using System;
+    using System.Diagnostics.CodeAnalysis;
     using System.Runtime.InteropServices;
 
     public static unsafe class IshtarUnsafe
     {
-        public static void* AsPointer<T>(ref T t) where T : class
+        public static void* AsPointer<T>([NotNull] ref T t) where T : class
         {
+            if (t == null) throw new ArgumentNullException(nameof(t));
+            GC.SuppressFinalize(t);
             var p = GCHandle.Alloc(t, GCHandleType.WeakTrackResurrection);
             return (void*)GCHandle.ToIntPtr(p);
         }
 
         public static T AsRef<T>(void* raw) where T : class
         {
+            if (raw == null) throw new ArgumentNullException(nameof(raw));
             var p = GCHandle.FromIntPtr((nint) raw);
-            return p.Target as T;
+            var r = p.Target as T;
+            GC.ReRegisterForFinalize(r ?? throw new InvalidOperationException());
+            return r;
         }
     }
 }
