9892b584bb96cf11c7c4f8efed1af18f31498b4d
Yuuki Wesp <ls-micro@ya.ru>
2021-06-27 00:23:08 +0300

    add ast structure for foreach statement #60

---
+++ lib/ast/syntax/ast/ForeachStatementSyntax.cs
@@ -0,0 +1,30 @@
+namespace mana.syntax
+{
+    using System.Collections.Generic;
+    using Sprache;
+
+    public class ForeachStatementSyntax : StatementSyntax, IPositionAware<ForeachStatementSyntax>
+    {
+        public LocalVariableDeclaration Variable { get; }
+        public ExpressionSyntax Expression { get; }
+        public StatementSyntax Statement { get; }
+
+
+        public override SyntaxType Kind => SyntaxType.ForEachStatement;
+
+        public override IEnumerable<BaseSyntax> ChildNodes => new List<BaseSyntax> { Variable, Expression, Statement };
+
+        public ForeachStatementSyntax(LocalVariableDeclaration declaration, ExpressionSyntax exp, StatementSyntax statement)
+        {
+            Variable = declaration;
+            Expression = exp;
+            Statement = statement;
+        }
+
+        public new ForeachStatementSyntax SetPos(Position startPos, int length)
+        {
+            base.SetPos(startPos, length);
+            return this;
+        }
+    }
+}
