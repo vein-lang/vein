d2d53fa4f899d5c26a51a3847f75e49f214c304c
Yuuki Wesp <ls-micro@ya.ru>
2021-07-01 22:35:16 +0300

    rework RuntimeToken #64

--- backend/Common/reflection/TypeName.cs
+++ backend/Common/reflection/TypeName.cs
@@ -2,39 +2,9 @@ namespace mana.runtime
 {
     using System;
     using System.Linq;
-    using System.Security;
     using System.Text.RegularExpressions;
     using extensions;
-
-    [Obsolete]
-    public record RuntimeToken(string text, ulong Value)
-    {
-        public static RuntimeToken Create(string id)
-            => new(id, getHashCode(id));
-        [SecurityCritical]
-        private static unsafe ulong getHashCode(string str)
-        {
-            fixed (char* chPtr1 = str)
-            {
-                var num1 = 0x1505UL;
-                var num2 = num1;
-                var chPtr2 = chPtr1;
-                ulong num3;
-                while ((num3 = *chPtr2) != 0)
-                {
-                    num1 = (num1 << 5) + num1 ^ num3;
-                    var num4 = (ulong) chPtr2[1];
-                    if (num4 != 0)
-                    {
-                        num2 = (num2 << 5) + num2 ^ num4;
-                        chPtr2 += 2;
-                    }
-                    else break;
-                }
-                return num1 + num2 * 0x5D588B65;
-            }
-        }
-    }
+    
     public class InvalidTypeNameException : Exception { public InvalidTypeNameException(string msg) : base(msg) { } }
     public record QualityTypeName(string fullName)
     {
---
+++ backend/mana.backend.ishtar.light/runtime/RuntimeToken.cs
@@ -0,0 +1,45 @@
+namespace ishtar
+{
+    using System.Runtime.InteropServices;
+
+    /// <summary>
+    ///  RuntimeToken:
+    ///     0xFFFF_____FFFF
+    ///         |        |
+    ///       ModuleID  EntityID
+    /// </summary>
+    public record RuntimeToken(uint Value)
+    {
+        public ushort ModuleID { get; }
+        public ushort ClassID { get; }
+
+        public RuntimeToken(ushort moduleID, ushort classID)
+            : this(new cast_uint(moduleID, classID).Value)
+        {
+            ModuleID = moduleID;
+            ClassID = classID;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        private readonly struct cast_uint
+        {
+            [FieldOffset(0)]
+            private readonly uint _result;
+            [FieldOffset(0)]
+            private readonly ushort _s1;
+            [FieldOffset(2)]
+            private readonly ushort _s2;
+
+
+            public cast_uint(ushort s1, ushort s2)
+            {
+                _result = 0;
+                _s1 = s1;
+                _s2 = s2;
+            }
+            // wtf resharper, _result is contains offset, why u are bully me?
+            // ReSharper disable once ConvertToAutoProperty
+            public uint Value => _result;
+        }
+    }
+}
