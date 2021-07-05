b57cc5f31bd3da4c0e1b2537aae4fa023ffa16b6
Yuuki Wesp <ls-micro@ya.ru>
2021-07-05 22:15:33 +0300

    add ast v2 project

---
+++ lib/mana.ast.v2/syntax/ManaSyntax.cs
@@ -0,0 +1,39 @@
+namespace mana.ast.v2.syntax
+{
+    using System;
+    using System.Linq.Expressions;
+    using Superpower;
+    using Superpower.Display;
+    using Superpower.Parsers;
+
+    public partial class ManaSyntax
+    {
+    }
+
+
+    public enum ManaTokenKind
+    {
+        [Token(Example = "{")]
+        LBracket,
+        [Token(Example = "}")]
+        RBracket,
+        [Token(Example = "[")]
+        LSquareBracket,
+        [Token(Example = "]")]
+        RSquareBracket,
+        [Token(Example = ":")]
+        Colon,
+        [Token(Example = ",")]
+        Comma,
+        [Token(Category="operator", Example = "+")]
+        Add,
+        [Token(Category = "operator", Example = "-")]
+        Sub,
+        [Token(Category = "operator", Example = "*")]
+        Mul,
+        [Token(Category = "operator", Example = "/")]
+        Div,
+        [Token(Category = "operator", Example = "^^")]
+        Pow,
+    }
+}
---
+++ lib/mana.ast.v2/tag.cs
@@ -0,0 +1,10 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("mana.backend.ishtar")]
+[assembly: InternalsVisibleTo("mana.backend.ishtar.generator")]
+[assembly: InternalsVisibleTo("mana.backend.clr")]
+[assembly: InternalsVisibleTo("mana.backend.llvm")]
+[assembly: InternalsVisibleTo("mana.backend.hashlink")]
+[assembly: InternalsVisibleTo("manac")]
+[assembly: InternalsVisibleTo("manalsp")]
+[assembly: InternalsVisibleTo("manac_test")]
