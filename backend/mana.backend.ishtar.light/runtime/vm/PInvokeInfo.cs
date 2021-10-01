171879235ce533f4e68089d21649cf01bc830ae9
Yuuki Wesp <ls-micro@ya.ru>
2021-10-01 14:10:21 +0300

    add implicit operator for PInvokeInfo

--- backend/mana.backend.ishtar.light/runtime/vm/PInvokeInfo.cs
+++ backend/mana.backend.ishtar.light/runtime/vm/PInvokeInfo.cs
@@ -6,5 +6,9 @@ namespace ishtar
         public void* Addr;
 
         public static PInvokeInfo New(void* p) => new() { Addr = p };
+
+
+        public static implicit operator void*(PInvokeInfo p) => p.Addr;
+        public static implicit operator PInvokeInfo(void* p) => New(p);
     }
 }
