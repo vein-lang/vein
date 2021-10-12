febb74362731329893939f1086a243fc30b1eefd
Yuuki Wesp <ls-micro@ya.ru>
2021-10-12 08:42:47 +0300

    add overloading for Transition

--- runtime/common/extensions/EtcExtensions.cs
+++ runtime/common/extensions/EtcExtensions.cs
@@ -9,5 +9,11 @@ namespace vein.extensions
             ft1(tuple.t1);
             ft2(tuple.t2);
         }
+        public static void Transition<T1, T2, T3>(this (T1 t1, T2 t2, T3 t3) tuple, Action<T1> ft1, Action<T2> ft2, Action<T3> ft3)
+        {
+            ft1(tuple.t1);
+            ft2(tuple.t2);
+            ft3(tuple.t3);
+        }
     }
 }
