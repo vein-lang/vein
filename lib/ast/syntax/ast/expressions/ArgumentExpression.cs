e6de0a044f77988a1c8083888aff40e765711045
Yuuki Wesp <ls-micro@ya.ru>
2021-10-17 20:36:56 +0300

    improvement ArgumentExpression

--- lib/ast/syntax/ast/expressions/ArgumentExpression.cs
+++ lib/ast/syntax/ast/expressions/ArgumentExpression.cs
@@ -1,23 +1,25 @@
 namespace vein.syntax
 {
+    using System;
     using Sprache;
 
     public class ArgumentExpression : ExpressionSyntax, IPositionAware<ArgumentExpression>
     {
+        [Obsolete("TODO")]
         public IdentifierExpression Identifier { get; set; }
+        [Obsolete("TODO")]
         public ExpressionSyntax Type { get; set; }
         public ExpressionSyntax Value { get; set; }
 
-        public ArgumentExpression(IOption<IdentifierExpression> id, IOption<ExpressionSyntax> t, ExpressionSyntax v)
-        {
-            this.Identifier = id?.GetOrDefault();
-            this.Type = t?.GetOrDefault();
-            this.Value = v;
-        }
+        public ArgumentExpression(ExpressionSyntax v)
+            => this.Value = v;
+
         public new ArgumentExpression SetPos(Position startPos, int length)
         {
             base.SetPos(startPos, length);
             return this;
         }
+
+        public override string ToString() => $"{Value}";
     }
 }
