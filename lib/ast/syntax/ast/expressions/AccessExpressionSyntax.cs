d9e16a7e7c79ca493ec19ddb5efb00bf22e29fe7
Yuuki Wesp <ls-micro@ya.ru>
2021-10-17 20:36:44 +0300

    add IndexerAccessExpressionSyntax, refactoring AccessExpressionSyntax

--- lib/ast/syntax/ast/expressions/AccessExpressionSyntax.cs
+++ lib/ast/syntax/ast/expressions/AccessExpressionSyntax.cs
@@ -1,15 +1,36 @@
 namespace vein.syntax
 {
+    using System.Linq.Expressions;
     using Sprache;
 
     public class AccessExpressionSyntax : BinaryExpressionSyntax, IPositionAware<AccessExpressionSyntax>
     {
-        public ExpressionSyntax Exp1, Exp2;
-        public ExpressionSyntax op;
+        public AccessExpressionSyntax(ExpressionSyntax left, ExpressionSyntax right)
+        {
+            this.Left = left;
+            this.Right = right;
+            this.OperatorType = ExpressionType.MemberAccess;
+        }
         public new AccessExpressionSyntax SetPos(Position startPos, int length)
         {
             base.SetPos(startPos, length);
             return this;
         }
+
+        public override string ToString() => $"{Left}.{Right}";
+    }
+
+    public class IndexerAccessExpressionSyntax : AccessExpressionSyntax, IPositionAware<IndexerAccessExpressionSyntax>
+    {
+        public IndexerAccessExpressionSyntax(ExpressionSyntax left, ExpressionSyntax right) : base(left, right)
+            => base.OperatorType = ExpressionType.ArrayIndex;
+
+        public new IndexerAccessExpressionSyntax SetPos(Position startPos, int length)
+        {
+            base.SetPos(startPos, length);
+            return this;
+        }
+
+        public override string ToString() => $"{Left}[{Right}]";
     }
 }
