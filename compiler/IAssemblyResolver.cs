9bbf6ae67a8c6c8e87e79aae6b5201ba9b644a89
Yuuki Wesp <ls-micro@ya.ru>
2021-10-30 03:22:27 +0300

    add link to CompilationTarget in AssemblyResolver

--- compiler/IAssemblyResolver.cs
+++ compiler/IAssemblyResolver.cs
@@ -1,10 +1,15 @@
 namespace vein
 {
+    using System;
     using compilation;
     using ishtar;
 
     public class AssemblyResolver : ModuleResolverBase
     {
-        protected override void debug(string s) => Log.Info(s);
+        private readonly CompilationTarget _target;
+        
+        public AssemblyResolver(CompilationTarget target) => _target = target;
+        public AssemblyResolver() {}
+        protected override void debug(string s) => Log.Info(s, _target);
     }
 }
