6bff83467a632e570aa400c5c09370e4f7fe35a6
Yuuki Wesp <ls-micro@ya.ru>
2021-11-19 03:18:14 +0300

    exclude from cover native export and default watchdog

--- runtime/ishtar.vm/DefaultWatchDog.cs
+++ runtime/ishtar.vm/DefaultWatchDog.cs
@@ -1,37 +1,38 @@
-namespace ishtar
+namespace ishtar;
+
+
+using System;
+using System.Threading;
+
+[ExcludeFromCodeCoverage]
+public class DefaultWatchDog : IWatchDog
 {
-    using System;
-    using System.Threading;
-
-    public class DefaultWatchDog : IWatchDog
+    private static readonly object guarder = new();
+    void IWatchDog.FastFail(WNE type, string msg, CallFrame frame)
     {
-        private static readonly object guarder = new();
-        void IWatchDog.FastFail(WNE type, string msg, CallFrame frame)
+        lock (guarder)
         {
-            lock (guarder)
-            {
-                var result = new NativeException {code = type, msg = msg, frame = frame};
-                Interlocked.Exchange(ref VM.CurrentException, result);
-            }
+            var result = new NativeException {code = type, msg = msg, frame = frame};
+            Interlocked.Exchange(ref VM.CurrentException, result);
         }
+    }
 
-        void IWatchDog.ValidateLastError()
+    void IWatchDog.ValidateLastError()
+    {
+        lock (guarder)
         {
-            lock (guarder)
+            if (VM.CurrentException is not null)
             {
-                if (VM.CurrentException is not null)
-                {
-                    CallFrame.FillStackTrace(VM.CurrentException.frame);
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    var err = $"native exception was thrown.\n\t" +
-                              $"[{VM.CurrentException.code}]\n\t" +
-                              $"'{VM.CurrentException.msg}'";
-                    if (VM.CurrentException.frame.exception is not null)
-                        err += $"\n{VM.CurrentException.frame.exception.stack_trace}";
-                    VM.println(err);
-                    Console.ForegroundColor = ConsoleColor.White;
-                    VM.halt();
-                }
+                CallFrame.FillStackTrace(VM.CurrentException.frame);
+                Console.ForegroundColor = ConsoleColor.Red;
+                var err = $"native exception was thrown.\n\t" +
+                          $"[{VM.CurrentException.code}]\n\t" +
+                          $"'{VM.CurrentException.msg}'";
+                if (VM.CurrentException.frame.exception is not null)
+                    err += $"\n{VM.CurrentException.frame.exception.stack_trace}";
+                VM.println(err);
+                Console.ForegroundColor = ConsoleColor.White;
+                VM.halt();
             }
         }
     }
--- runtime/ishtar.vm/NativeExports.cs
+++ runtime/ishtar.vm/NativeExports.cs
@@ -1,81 +1,82 @@
-namespace ishtar
+namespace ishtar;
+
+
+using System.Linq;
+using System.Runtime.InteropServices;
+using vein.runtime;
+
+[ExcludeFromCodeCoverage]
+public static unsafe class NativeExports
 {
-    using System.Linq;
-    using System.Runtime.InteropServices;
-    using vein.runtime;
-
-    public static unsafe class NativeExports
+    [UnmanagedCallersOnly]
+    public static void VM_INIT()
     {
-        [UnmanagedCallersOnly]
-        public static void VM_INIT()
-        {
-            IshtarCore.INIT();
-            INIT_VTABLES();
-            IshtarGC.INIT();
-            FFI.INIT();
+        IshtarCore.INIT();
+        INIT_VTABLES();
+        IshtarGC.INIT();
+        FFI.INIT();
 
-            AppVault.CurrentVault = new AppVault("app");
+        AppVault.CurrentVault = new AppVault("app");
+    }
+
+    [UnmanagedCallersOnly]
+    public static void VM_VALIDATE_LAST_ERROR() => VM.ValidateLastError();
+
+    [UnmanagedCallersOnly]
+    public static stackval* VM_EXECUTE_METHOD(Types.FrameRef* frame)
+    {
+        var type = AppVault.CurrentVault.GlobalFindType(*frame->runtime_token);
+
+        if (type is null)
+            return null;
+
+        if (type.Methods.Count >= frame->index)
+            return null;
+
+        var method = type.Methods[frame->index] as RuntimeIshtarMethod;
+
+        if (method is not { } or { IsExtern: true } or { IsStatic: false })
+            return null;
+
+
+        var callframe = new CallFrame()
+        {
+            args = frame->args,
+            level = 0,
+            method = method
+        };
+
+        VM.exec_method(callframe);
+
+        return callframe.returnValue;
+    }
+
+    private static void INIT_VTABLES()
+    {
+        foreach (var @class in VeinCore.All.OfType<RuntimeIshtarClass>())
+            @class.init_vtable();
+    }
+
+
+    public static class Types
+    {
+        public struct FrameRef
+        {
+            public stackval* args;
+            public RuntimeTokenRef* runtime_token;
+            public int index;
         }
 
-        [UnmanagedCallersOnly]
-        public static void VM_VALIDATE_LAST_ERROR() => VM.ValidateLastError();
-
-        [UnmanagedCallersOnly]
-        public static stackval* VM_EXECUTE_METHOD(Types.FrameRef* frame)
+        public struct RuntimeTokenRef
         {
-            var type = AppVault.CurrentVault.GlobalFindType(*frame->runtime_token);
-
-            if (type is null)
-                return null;
-
-            if (type.Methods.Count >= frame->index)
-                return null;
-
-            var method = type.Methods[frame->index] as RuntimeIshtarMethod;
-
-            if (method is not { } or { IsExtern: true } or { IsStatic: false })
-                return null;
+            public ushort ModuleID;
+            public ushort ClassID;
 
 
-            var callframe = new CallFrame()
-            {
-                args = frame->args,
-                level = 0,
-                method = method
-            };
-
-            VM.exec_method(callframe);
-
-            return callframe.returnValue;
-        }
-
-        private static void INIT_VTABLES()
-        {
-            foreach (var @class in VeinCore.All.OfType<RuntimeIshtarClass>())
-                @class.init_vtable();
-        }
-
-
-        public static class Types
-        {
-            public struct FrameRef
-            {
-                public stackval* args;
-                public RuntimeTokenRef* runtime_token;
-                public int index;
-            }
-
-            public struct RuntimeTokenRef
-            {
-                public ushort ModuleID;
-                public ushort ClassID;
-
-
-                public static implicit operator RuntimeTokenRef(RuntimeToken tok)
-                    => new() { ClassID = tok.ClassID, ModuleID = tok.ModuleID };
-                public static implicit operator RuntimeToken(RuntimeTokenRef tok)
-                    => new(tok.ModuleID, tok.ClassID);
-            }
+            public static implicit operator RuntimeTokenRef(RuntimeToken tok)
+                => new() { ClassID = tok.ClassID, ModuleID = tok.ModuleID };
+            public static implicit operator RuntimeToken(RuntimeTokenRef tok)
+                => new(tok.ModuleID, tok.ClassID);
         }
     }
 }
