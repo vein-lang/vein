9360cad51dc195787f8d4af43cf4f85b631c51af
Yuuki Wesp <ls-micro@ya.ru>
2021-11-25 23:16:31 +0300

    add ImmortalObject

--- runtime/ishtar.vm/runtime/IshtarUnsafe.cs
+++ runtime/ishtar.vm/runtime/IshtarUnsafe.cs
@@ -23,4 +23,27 @@ namespace ishtar
             return r;
         }
     }
+
+    public ref struct ImmortalObject<T> where T : class
+    {
+        public IntPtr Pointer { get; private set; }
+        public void Create([NotNull] T t)
+        {
+            if (t == null) throw new ArgumentNullException(nameof(t));
+
+            GC.SuppressFinalize(t);
+            var p = GCHandle.Alloc(t, GCHandleType.WeakTrackResurrection);
+            this.Pointer = GCHandle.ToIntPtr(p);
+        }
+
+        public T Value => GCHandle.FromIntPtr((nint)Pointer).Target as T;
+
+        public void Delete()
+        {
+            if (Pointer == default) throw new ArgumentNullException(nameof(Pointer));
+            var p = GCHandle.FromIntPtr((nint) Pointer);
+            var r = p.Target as T;
+            GC.ReRegisterForFinalize(r ?? throw new InvalidOperationException());
+        }
+    }
 }
