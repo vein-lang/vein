0fc744ecd8b43a1f7ed79f98f280ea648576da67
Yuuki Wesp <ls-micro@ya.ru>
2021-11-27 10:11:46 +0300

    add PackageManifest, PackageMetadata

---
+++ lib/projectsystem/PackageManifest.cs
@@ -0,0 +1,44 @@
+namespace vein.project;
+
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using NuGet.Versioning;
+
+public record PackageManifest
+{
+    [JsonProperty("id")]
+    public string Name { get; set; }
+    [JsonProperty("version")]
+    public NuGetVersion Version { get; set; }
+    [JsonProperty("description")]
+    public string Description { get; set; }
+    [JsonProperty("authors")]
+    public List<string> Authors { get; set; } = new ();
+    [JsonProperty("icon")]
+    public string Icon { get; set; }
+    [JsonProperty("license")]
+    public string License { get; set; }
+    [JsonProperty("preview")]
+    public bool IsPreview { get; set; }
+    [JsonProperty("bugs")]
+    public Uri BugUrl { get; set; }
+    [JsonProperty("homepage")]
+    public Uri HomepageUrl { get; set; }
+    [JsonProperty("repository")]
+    public Uri Repository { get; set; }
+    [JsonProperty("keywords")]
+    public List<string> Keywords { get; set; } = new ();
+    [JsonProperty("categories")]
+    public List<string> Categories { get; set; } = new ();
+    [JsonProperty("dependencies")]
+    public List<PackageReference> Dependencies { get; set; } = new ();
+}
+
+public record PackageMetadata
+{
+    [JsonProperty("publish_date")]
+    public DateTimeOffset PublishDate { get; set; }
+    [JsonProperty("id")]
+    public string ID { get; set; }
+}
