abbcd2db0c979ead6e696010575c7a926f1e0d43
Yuuki Wesp <ls-micro@ya.ru>
2021-12-20 07:41:47 +0300

    add i_call_String_Equal

--- runtime/ishtar.vm/__builtin/B_String.cs
+++ runtime/ishtar.vm/__builtin/B_String.cs
@@ -13,6 +13,11 @@ namespace ishtar
                 .AsNative((delegate*<CallFrame, IshtarObject**, IshtarObject*>)&Concat)
                 .AddInto(table, x => x.Name);
 
+            new RuntimeIshtarMethod("i_call_String_Equal", Private | Static | Extern,
+                    ("v1", TYPE_STRING), ("v2", TYPE_STRING))
+                .AsNative((delegate*<CallFrame, IshtarObject**, IshtarObject*>)&StrEqual)
+                .AddInto(table, x => x.Name);
+
             new RuntimeIshtarMethod("i_call_String_Trim_Start", Private | Static | Extern,
                     ("v1", TYPE_STRING))
                 .AsNative((delegate*<CallFrame, IshtarObject**, IshtarObject*>)&TrimStart)
@@ -45,6 +50,25 @@ namespace ishtar
             return IshtarMarshal.ToIshtarObject(result, frame);
         }
 
+        [IshtarExportFlags(Private | Static)]
+        [IshtarExport(2, "i_call_String_Equal")]
+        public static IshtarObject* StrEqual(CallFrame frame, IshtarObject** args)
+        {
+            var i_str1 = args[0];
+            var i_str2 = args[1];
+
+            FFI.StaticValidate(frame, &i_str1);
+            FFI.StaticValidate(frame, &i_str2);
+            FFI.StaticTypeOf(frame, &i_str1, TYPE_STRING);
+            FFI.StaticTypeOf(frame, &i_str2, TYPE_STRING);
+
+            var str1 = IshtarMarshal.ToDotnetString(i_str1, frame);
+            var str2 = IshtarMarshal.ToDotnetString(i_str2, frame);
+
+            var result = str1.Equals(str2);
+
+            return IshtarMarshal.ToIshtarObject(result, frame);
+        }
 
         public static IshtarObject* TemplateFunctionApply(CallFrame frame, IshtarObject** args, Func<string, string> apply)
         {
