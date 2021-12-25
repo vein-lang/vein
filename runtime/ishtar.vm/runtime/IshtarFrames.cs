fb4d8cc10221e12dc35073549e6cccb26c75a5f7
Yuuki Wesp <ls-micro@ya.ru>
2021-12-26 02:21:08 +0300

    add entrypoint call frame

--- runtime/ishtar.vm/runtime/IshtarFrames.cs
+++ runtime/ishtar.vm/runtime/IshtarFrames.cs
@@ -11,6 +11,9 @@ public static class IshtarFrames
     public static CallFrame VTableFrame(VeinClass clazz) => new CallFrame()
     {
         method = RuntimeIshtarMethod.DefineEmptySystemMethod("loader", clazz),
-
+    };
+    public static CallFrame EntryPoint = new CallFrame()
+    {
+        method = RuntimeIshtarMethod.DefineEmptySystemMethod("ishtar_entry")
     };
 }
