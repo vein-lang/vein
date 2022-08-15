da40e6ec9255d065fd33e20adf8e6909e5d905b5
Yuuki Wesp <ls-micro@ya.ru>
2022-08-15 04:27:29 +0300

    add i_call_NAPI_LoadNative

---
+++ runtime/ishtar.vm/__builtin/B_NAPI.cs
@@ -0,0 +1,59 @@
+namespace ishtar;
+
+using static vein.runtime.MethodFlags;
+using static vein.runtime.VeinTypeCode;
+
+public static unsafe class B_NAPI
+{
+    [IshtarExport(1, "i_call_NAPI_LoadNative")]
+    [IshtarExportFlags(Public | Static)]
+    public static IshtarObject* LoadNativeLibrary(CallFrame current, IshtarObject** args)
+    {
+        var arg1 = args[0];
+
+        if (arg1 == null)
+        {
+            current.ThrowException(KnowTypes.NullPointerException(current));
+            return IshtarObject.NullPointer;
+        }
+
+        FFI.StaticValidate(current, &arg1);
+        FFI.StaticTypeOf(current, &arg1, TYPE_STRING);
+        var @class = arg1->decodeClass();
+
+        var libPath = IshtarMarshal.ToDotnetString(arg1, current);
+        var libFile = new FileInfo(libPath);
+
+        if (!libFile.Exists)
+        {
+            current.ThrowException(KnowTypes.VeinLang.FileNotFoundFault(current));
+            return IshtarObject.NullPointer;
+        }
+
+        var result = NativeProviderLoader.TryLoadFile(libFile, out var h);
+
+        if (!result)
+        {
+            current.ThrowException(KnowTypes.NativeFault(current));
+            return IshtarObject.NullPointer;
+        }
+
+
+        var handleClass = KnowTypes.VeinLang.Native.NativeHandle(current);
+        var handleObj = IshtarGC.AllocObject(handleClass);
+
+        var wrapper = new KnowTypes.WrappedTypes.S_NativeHandle(handleObj, current);
+
+        wrapper.Handle = h;
+
+        return handleObj;
+    }
+
+
+    public static void InitTable(Dictionary<string, RuntimeIshtarMethod> table)
+    {
+        new RuntimeIshtarMethod("i_call_NAPI_LoadNative", Public | Static | Extern)
+            .AsNative((delegate*<CallFrame, IshtarObject**, IshtarObject*>)&LoadNativeLibrary)
+            .AddInto(table, x => x.Name);
+    }
+}
