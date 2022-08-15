317027d1f745c765ed4ca6b53657df95cd22c465
Yuuki Wesp <ls-micro@ya.ru>
2022-08-15 04:33:21 +0300

    add logic for loading native libraries and their symbols

--- runtime/ishtar.vm/FFI/FFI.cs
+++ runtime/ishtar.vm/FFI/FFI.cs
@@ -2,6 +2,7 @@ namespace ishtar
 {
     using System.Collections.Generic;
     using System.Diagnostics;
+    using System.Runtime.InteropServices;
     using vein.runtime;
 
     public static unsafe class FFI
@@ -21,6 +22,7 @@ namespace ishtar
             B_Type.InitTable(method_table);
             B_Field.InitTable(method_table);
             B_Function.InitTable(method_table);
+            B_NAPI.InitTable(method_table);
         }
 
 
@@ -69,5 +71,72 @@ namespace ishtar
 
         public static RuntimeIshtarMethod GetMethod(string FullName)
             => method_table.GetValueOrDefault(FullName);
+
+
+        public static void LinkExternalNativeLibrary(string inportTarget, string fnName,
+            RuntimeIshtarMethod importCaller)
+        {
+            importCaller.PIInfo = PInvokeInfo.New(null) with { iflags = (ushort)PInvokeInfo.Flags.EXTERNAL };
+
+            var entity = new NativeImportEntity(inportTarget, fnName, importCaller);
+
+            if (importCaller.Owner is not RuntimeIshtarClass clazz)
+                throw null;
+            clazz.nativeImports.Add(entity);
+        }
+
+
+
+        
+        // ==================
+        // TODO, move to outside
+        // ==================
+
+        private static readonly Dictionary<string, NativeImportCache> _cache = new ();
+
+        public static void LoadNativeLibrary(NativeImportEntity entity, CallFrame frame)
+        {
+            if (_cache.ContainsKey(entity.entry))
+                return;
+
+            var result = NativeProviderLoader.TryLoadFile(new FileInfo(entity.entry), out var handle);
+
+            if (!result)
+            {
+                VM.FastFail(WNE.NATIVE_LIBRARY_COULD_NOT_LOAD, $"{entity.entry}", frame);
+                return;
+            }
+
+            _cache[entity.entry] = new NativeImportCache(entity.entry, handle);
+        }
+
+        public static void LoadNativeSymbol(NativeImportEntity entity, CallFrame frame)
+        {
+            var cached = _cache[entity.entry];
+
+            if (cached.ImportedSymbols.ContainsKey(entity.fn))
+                return;
+
+            try
+            {
+                var symbol = NativeLibrary.GetExport(cached.handle, entity.fn);
+
+                cached.ImportedSymbols.Add(entity.fn, symbol);
+            }
+            catch
+            {
+                VM.FastFail(WNE.NATIVE_LIBRARY_SYMBOL_COULD_NOT_FOUND, $"{entity.entry}::{entity.fn}", frame);
+            }
+        }
+    }
+
+    public record NativeImportCache(string entry, nint handle)
+    {
+        public Dictionary<string, nint> ImportedSymbols = new Dictionary<string, nint>();
+    }
+
+    public record NativeImportEntity(string entry, string fn, RuntimeIshtarMethod importer)
+    {
+        public VeinModule Module => importer.Owner.Owner;
     }
 }
