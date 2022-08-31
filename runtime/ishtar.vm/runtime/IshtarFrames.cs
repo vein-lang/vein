3746f62fbd82e8d1befef3cbc07bb867a26386aa
Yuuki Wesp <ls-micro@ya.ru>
2022-08-31 17:10:31 +0300

    add frame for NativeLoader

--- runtime/ishtar.vm/runtime/IshtarFrames.cs
+++ runtime/ishtar.vm/runtime/IshtarFrames.cs
@@ -28,4 +28,9 @@ public static class IshtarFrames
     {
         method = RuntimeIshtarMethod.DefineEmptySystemMethod(".jit")
     };
+
+    public static CallFrame NativeLoader() => new CallFrame()
+    {
+        method = RuntimeIshtarMethod.DefineEmptySystemMethod(".ffi")
+    };
 }
