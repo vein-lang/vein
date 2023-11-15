65d10195c30670859d52741e3d61516fb5f0fca0
Yuuki Wesp <ls-micro@ya.ru>
2023-11-15 23:24:04 +0300

    add smart pointer for array like pointers

---
+++ runtime/ishtar.vm/SmartPointer.cs
@@ -0,0 +1,14 @@
+namespace ishtar;
+
+public readonly unsafe struct SmartPointer<T>(ushort size, CallFrame frame,
+    delegate*<CallFrame, int, T*> allocator,
+    delegate*<CallFrame, T*, int, void> free) : IDisposable where T : unmanaged
+{
+    public readonly T* Ref = allocator(frame, size);
+    public readonly ushort size = size;
+
+    public void Dispose() => free(frame, Ref, size);
+
+    public ref T this[int index] => ref Ref[index];
+    public ref T this[uint index] => ref Ref[index];
+}
--- runtime/ishtar.vm/stackval.cs
+++ runtime/ishtar.vm/stackval.cs
@@ -1,18 +1,28 @@
 namespace ishtar
 {
-    using System;
     using vein.runtime;
 
     public struct stackval
     {
         public stack_union data;
         public VeinTypeCode type;
-
-        public static stackval[] Alloc(int size) => GC.AllocateArray<stackval>(size, true);
-
-
+        
         public void validate(CallFrame frame, VeinTypeCode typeCode) =>
             VirtualMachine.Assert(type == VeinTypeCode.TYPE_ARRAY, WNE.TYPE_MISMATCH,
                 $"stack type mismatch, current: '{type}', expected: '{typeCode}'. opcode: '{frame.last_ip}'", frame);
+
+
+        public static unsafe SmartPointer<stackval> Allocate(CallFrame frame, short size)
+            => Allocate(frame, (ushort)size);
+
+        public static unsafe SmartPointer<stackval> Allocate(CallFrame frame, ushort size)
+        {
+            static stackval* alloc(CallFrame frame, int size)
+                => frame.vm.GC.AllocateStack(frame, size);
+            static void free(CallFrame frame, stackval* stack, int size)
+                => frame.vm.GC.FreeStack(frame, stack, size);
+
+            return new SmartPointer<stackval>(size, frame, &alloc, &free);
+        }
     }
 }
