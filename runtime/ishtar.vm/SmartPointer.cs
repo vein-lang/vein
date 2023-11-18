e11ba813ec2f4e7da07e52e9b4bd27d5b2abe6b5
Yuuki Wesp <ls-micro@ya.ru>
2023-11-18 06:06:04 +0300

    add debug original address for changes checking

--- runtime/ishtar.vm/SmartPointer.cs
+++ runtime/ishtar.vm/SmartPointer.cs
@@ -4,9 +4,14 @@ public readonly unsafe struct SmartPointer<T>(ushort size, CallFrame frame,
     delegate*<CallFrame, int, T*> allocator,
     delegate*<CallFrame, T*, int, void> free) : IDisposable where T : unmanaged
 {
+#if DEBUG
+    private readonly nint[] OriginalAddress = new nint[1];
+
+    internal void CaptureAddress() => OriginalAddress[0] = (nint)Ref;
+#endif
     public readonly T* Ref = allocator(frame, size);
     public readonly ushort size = size;
-
+    
     public void Dispose()
     {
         if (!IsNull()) free(frame, Ref, size);
