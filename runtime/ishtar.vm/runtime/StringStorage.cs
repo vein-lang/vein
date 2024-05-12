e99a600f91d63aa2a62bb2e0d416724b83d0a09c
Yuuki Wesp <ls-micro@ya.ru>
2024-05-12 03:37:22 +0300

    rework string storage

--- runtime/ishtar.vm/runtime/StringStorage.cs
+++ runtime/ishtar.vm/runtime/StringStorage.cs
@@ -7,25 +7,29 @@ namespace ishtar
         internal static readonly Dictionary<string, ulong> storage_r = new();
         internal static readonly Dictionary<ulong, string> storage_l = new();
 
-        public static StrRef* Intern(string value)
+        public static InternedString* Intern(string value)
         {
-            if (storage_r.ContainsKey(value))
-                return (StrRef*)storage_r[value];
+            if (storage_r.TryGetValue(value, out ulong value1))
+                return (InternedString*)value1;
             var p = (ulong)storage_r.Count + 1;
             storage_r.Add(value, p);
             storage_l.Add(p, value);
-            return (StrRef*)p;
+            return (InternedString*)p;
         }
 
-        public static string GetString(StrRef* p, CallFrame frame)
+        public static string GetString(InternedString* p, CallFrame frame)
         {
             ForeignFunctionInterface.StaticValidate(p, frame);
-            if (!storage_l.ContainsKey((ulong)p))
-            {
-                frame.vm.FastFail(WNE.ACCESS_VIOLATION, "Pointer incorrect.", frame);
-                return null;
-            }
-            return storage_l[(ulong)p];
+            if (storage_l.ContainsKey((ulong)p))
+                return storage_l[(ulong)p];
+            frame.vm.FastFail(WNE.ACCESS_VIOLATION, "Pointer incorrect.", frame);
+            return null;
         }
+
+        public static string GetStringUnsafe(InternedString* p)
+            => !storage_l.ContainsKey((ulong)p) ? null : storage_l[(ulong)p];
     }
+
+
+    public struct InternedString(ulong id);
 }
