7ba3c8e5da717cc19be508e54b9fde90a1578c78
Yuuki Wesp <ls-micro@ya.ru>
2024-05-15 08:43:26 +0300

    rework frames allocation, fixes duplications

--- runtime/ishtar.vm/runtime/IshtarFrames.cs
+++ runtime/ishtar.vm/runtime/IshtarFrames.cs
@@ -1,40 +1,44 @@
 // ReSharper disable MethodNameNotMeaningful
 namespace ishtar;
 
+using vein.runtime;
+
 public unsafe class IshtarFrames(VirtualMachine vm)
 {
     public CallFrame ModuleLoaderFrame = new(vm)
     {
-        method = vm.DefineEmptySystemMethod(".module")
+        method = vm.DefineEmptySystemMethod("#module")
     };
 
     public CallFrame VTableFrame(RuntimeIshtarClass* clazz) => new(vm)
     {
-        method = vm.DefineEmptySystemMethod(".type", clazz),
+        method = clazz->DefineMethod($"#type()", clazz, MethodFlags.Special)
     };
 
-    public CallFrame StaticCtor(RuntimeIshtarClass* clazz) => new(vm)
-    {
-        method = vm.DefineEmptySystemMethod(".static_ctor", clazz),
-    };
+    public CallFrame StaticCtor(RuntimeIshtarClass* clazz) =>
+        new(vm)
+        {
+            method = clazz->DefineMethod("#static_ctor()", null, MethodFlags.Special | MethodFlags.Static)
+        };
+
 
     public CallFrame EntryPoint = new(vm)
     {
         method = vm.DefineEmptySystemMethod("ishtar_entry")
     };
 
-    public CallFrame Jit() => new(vm)
+    public CallFrame Jit = new(vm)
     {
-        method = vm.DefineEmptySystemMethod(".jit")
+        method = vm.DefineEmptySystemMethod("#jit")
     };
 
-    public CallFrame GarbageCollector() => new(vm)
+    public CallFrame GarbageCollector = new(vm)
     {
-        method = vm.DefineEmptySystemMethod(".gc")
+        method = vm.DefineEmptySystemMethod("#gc")
     };
 
-    public CallFrame NativeLoader() => new(vm)
+    public CallFrame NativeLoader = new(vm)
     {
-        method = vm.DefineEmptySystemMethod(".ffi")
+        method = vm.DefineEmptySystemMethod("#ffi")
     };
 }
