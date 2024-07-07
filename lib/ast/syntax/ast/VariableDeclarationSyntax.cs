4d629c72a629205b7041df6a2c5b156370eaa1ef
Yuuki Wesp <ls-micro@ya.ru>
2024-07-07 07:44:23 +0300

    refactoring

--- lib/ast/syntax/ast/VariableDeclarationSyntax.cs
+++ lib/ast/syntax/ast/VariableDeclarationSyntax.cs
@@ -1,42 +1,40 @@
-namespace vein.syntax
+namespace vein.syntax;
+
+using System.Collections.Generic;
+using System.Linq;
+using Sprache;
+
+public class QualifiedExpressionStatement(ExpressionSyntax exp)
+    : StatementSyntax, IPositionAware<QualifiedExpressionStatement>
 {
-    using System.Collections.Generic;
-    using System.Linq;
-    using Sprache;
+    public readonly ExpressionSyntax Value = exp;
 
-    public class QualifiedExpressionStatement : StatementSyntax, IPositionAware<QualifiedExpressionStatement>
+    public new QualifiedExpressionStatement SetPos(Position startPos, int length)
     {
-        public readonly ExpressionSyntax Value;
-
-        public QualifiedExpressionStatement(ExpressionSyntax exp) => Value = exp;
-
-        public new QualifiedExpressionStatement SetPos(Position startPos, int length)
-        {
-            base.SetPos(startPos, length);
-            return this;
-        }
-
-        public override string ExpressionString => Value.ExpressionString;
+        base.SetPos(startPos, length);
+        return this;
     }
 
-    public class VariableDeclarationSyntax : StatementSyntax, IPositionAware<VariableDeclarationSyntax>
+    public override string ExpressionString => Value.ExpressionString;
+}
+
+public class VariableDeclarationSyntax : StatementSyntax, IPositionAware<VariableDeclarationSyntax>
+{
+    public override SyntaxType Kind => SyntaxType.VariableDeclaration;
+
+    public override IEnumerable<BaseSyntax> ChildNodes =>
+        GetNodes(Type).Concat(new[] { Variable }).Where(n => n != null);
+
+    public TypeExpression Type { get; set; }
+
+    public IdentifierExpression Variable { get; set; }
+
+    public ExpressionSyntax ExpressionValue { get; set; }
+
+
+    public new VariableDeclarationSyntax SetPos(Position startPos, int length)
     {
-        public override SyntaxType Kind => SyntaxType.VariableDeclaration;
-
-        public override IEnumerable<BaseSyntax> ChildNodes =>
-            GetNodes(Type).Concat(new[] { Variable }).Where(n => n != null);
-
-        public TypeExpression Type { get; set; }
-
-        public IdentifierExpression Variable { get; set; }
-
-        public ExpressionSyntax ExpressionValue { get; set; }
-
-
-        public new VariableDeclarationSyntax SetPos(Position startPos, int length)
-        {
-            base.SetPos(startPos, length);
-            return this;
-        }
+        base.SetPos(startPos, length);
+        return this;
     }
 }
