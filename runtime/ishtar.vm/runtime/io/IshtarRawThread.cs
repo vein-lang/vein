1a177fecc97fe897537abad592af989621404c1c
Yuuki Wesp <ls-micro@ya.ru>
2024-07-09 23:35:27 +0300

    add name for raw thread

--- runtime/ishtar.vm/runtime/io/IshtarRawThread.cs
+++ runtime/ishtar.vm/runtime/io/IshtarRawThread.cs
@@ -6,9 +6,11 @@ using libuv;
 public readonly unsafe struct IshtarRawThread(
     LibUV.uv_thread_t threadId,
     delegate*<IshtarRawThread*, void> frame,
-    RuntimeIshtarModule* mainModule)
+    RuntimeIshtarModule* mainModule,
+    InternedString* name)
 {
     public readonly RuntimeIshtarModule* MainModule = mainModule;
     public readonly LibUV.uv_thread_t threadId = threadId;
     public readonly delegate*<IshtarRawThread*, void> callFrame = frame;
+    public readonly InternedString* Name = name;
 }
