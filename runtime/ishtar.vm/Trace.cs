96ec1b668786269b13b36efcfd393ea60f37e55e
Yuuki Wesp <ls-micro@ya.ru>
2024-07-09 23:34:19 +0300

    add Dump into ishtarTrace

--- runtime/ishtar.vm/Trace.cs
+++ runtime/ishtar.vm/Trace.cs
@@ -22,6 +22,85 @@ internal struct IshtarTrace()
     public unsafe void signal_state(OpCodeValue ip, CallFrame current, TimeSpan cycleDelay, stackval currentStack)
         => IshtarSharedDebugData.SetState(new IshtarState($"{ip}", current.method->Name, cycleDelay, $"{currentStack.type}", current.level));
 
+    public static unsafe object Dump(RuntimeIshtarModule* module)
+    {
+        var classes = new Dictionary<string, object>();
+
+        module->class_table->ForEach(x =>
+        {
+            classes.Add(x->Name, Dump(x));
+        });
+
+        return new
+        {
+            Name = module->Name,
+            Version = module->Version,
+            classes
+        };
+    }
+
+    public static unsafe object Dump(RuntimeIshtarClass* clazz)
+    {
+        var methods = new Dictionary<string, object>();
+        var fields = new Dictionary<string, object>();
+        var aspects = new Dictionary<string, object>();
+
+        var parentName = clazz->Parent == null ? null : clazz->Parent->Name;
+
+        clazz->Methods->ForEach(x =>
+        {
+            methods.Add(x->Name, Dump(x));
+        });
+
+
+        return new
+        {
+            Name = clazz->Name,
+            FullName = clazz->FullName->ToString(),
+            Flags = clazz->Flags,
+            ID = clazz->ID,
+            Parent = parentName,
+            methods,
+            fields,
+            aspects
+        };
+    }
+
+    public static unsafe object Dump(RuntimeIshtarMethod* method)
+    {
+        string readBytes(uint* code, uint codeSize)
+        {
+            var strBuilder = new StringBuilder();
+            var current = 0;
+            while (current < codeSize)
+            {
+                strBuilder.Append($"0x{(*code + current):X} ");
+                current++;
+            }
+            return strBuilder.ToString();
+        }
+
+        return new
+        {
+            Name = method->Name,
+            Flags = method->Flags,
+            ReturnType = new
+            {
+                TypeCode = method->ReturnType->TypeCode,
+                Name = method->ReturnType->Name,
+                ID = method->ReturnType->ID
+            },
+            Header = new
+            {
+                compiled_func_ref = method->PIInfo.compiled_func_ref,
+                extern_function_declaration = method->PIInfo.extern_function_declaration.Name,
+                jitted_wrapper = method->PIInfo.jitted_wrapper.Name,
+                code_size = method->Header == null ? 0 : method->Header->code_size,
+                body = method->Header == null ? "empty" : readBytes(method->Header->code, method->Header->code_size)
+            }
+        };
+    }
+
     public void console_std_write(string s)
     {
 #if DEBUG
