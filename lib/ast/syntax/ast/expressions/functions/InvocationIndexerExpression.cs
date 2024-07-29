98a6e8ae5abbd3acea1f161df321304052620502
Yuuki Wesp <ls-micro@ya.ru>
2024-07-29 07:45:09 +0300

    format InvocationExpression

--- lib/ast/syntax/ast/expressions/functions/InvocationIndexerExpression.cs
+++ lib/ast/syntax/ast/expressions/functions/InvocationIndexerExpression.cs
@@ -1,22 +1,20 @@
 namespace vein.syntax;
 
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Sprache;
 using extensions;
 
-public class ArgumentListExpression : ExpressionSyntax, IPositionAware<ArgumentListExpression>
+public class ArgumentListExpression(IEnumerable<ExpressionSyntax> args) 
+    : ExpressionSyntax, IPositionAware<ArgumentListExpression>
 {
-    public ExpressionSyntax[] Arguments { get; set; }
-
-    public ArgumentListExpression(IEnumerable<ExpressionSyntax> args)
-        => this.Arguments = args.ToArray();
+    public ExpressionSyntax[] Arguments { get; set; } = args.ToArray();
 
     public new ArgumentListExpression SetPos(Position startPos, int length)
     {
         base.SetPos(startPos, length);
         return this;
     }
-
-    public override string ToString() => $"{Arguments.Select(x => x.ToString()).Join(", ")}";
+    public override string ToString() => $"[ArgumentListExpression ({Arguments.Select(x => x.ToString()).Join(", ")})]";
 }
