13129e4a082ee320af71226d152bcad5bc1fabea
Yuuki Wesp <ls-micro@ya.ru>
2024-08-03 16:34:52 +0300

    add installing\update\uninstalling workloads

---
+++ tools/rune-cli/cmd/UninstallWorkloadCommand.cs
@@ -0,0 +1,67 @@
+namespace vein.cmd;
+
+using System.ComponentModel;
+using project;
+using Spectre.Console;
+using Spectre.Console.Cli;
+using styles;
+
+[ExcludeFromCodeCoverage]
+public class UninstallWorkloadCommandSettings : CommandSettings
+{
+    [Description("A package name.")]
+    [CommandArgument(0, "[PACKAGE]")]
+    public required string PackageName { get; set; }
+}
+public class UninstallWorkloadCommand : AsyncCommandWithProgress<UninstallWorkloadCommandSettings>
+{
+    public static readonly DirectoryInfo WorkloadDirectory = SecurityStorage.RootFolder.SubDirectory("workloads");
+    public static readonly Uri VEIN_GALLERY = new("https://api.vein-lang.org/");
+    public static readonly ShardStorage Storage = new();
+    public SymlinkCollector Symlink = new(SecurityStorage.RootFolder);
+
+    public override async Task<int> ExecuteAsync(ProgressContext context, UninstallWorkloadCommandSettings settings)
+    {
+        using var task = context.AddTask($"delete [orange3]'{settings.PackageName}'[/] workload...")
+            .IsIndeterminate();
+        await Task.Delay(1000);
+        var name = settings.PackageName;
+
+        if (!WorkloadDirectory
+                .SubDirectory(name).Exists)
+        {
+            task.FailTask();
+            Log.Error($"Workload package [orange3]'{name}'[/] not installed.");
+            return -1;
+        }
+
+        var version = await WorkloadDirectory
+            .SubDirectory(name).File("latest").ReadToEndAsync();
+        var tagFolder = WorkloadDirectory
+            .SubDirectory(name)
+            .SubDirectory(version);
+
+        var manifest = await WorkloadManifest.OpenAsync(tagFolder.File("manifest.json"));
+
+
+        foreach (var (key, pkg) in manifest.Packages)
+        {
+            foreach (var @base in pkg.Definition)
+            {
+                if (@base is WorkloadPackageTool tool)
+                {
+                    var file = new FileInfo(Symlink.ToExec(tool.ExecPath));
+                    Symlink.DeleteSymlink(string.IsNullOrEmpty(tool.OverrideName) ?
+                        Path.GetFileNameWithoutExtension(file.Name) :
+                        tool.OverrideName);
+                }
+                else
+                    throw new NotSupportedException();
+            }
+        }
+
+        tagFolder.Delete(true);
+
+        return 0;
+    }
+}
---
+++ tools/rune-cli/cmd/UpdateWorkloadCommand.cs
@@ -0,0 +1,50 @@
+namespace vein.cmd;
+
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Spectre.Console;
+using Spectre.Console.Cli;
+
+[ExcludeFromCodeCoverage]
+public class UpdateWorkloadCommandSettings : CommandSettings
+{
+    [Description("A package name.")]
+    [CommandArgument(0, "[PACKAGE]")]
+    public required string PackageName { get; set; }
+
+    [Description("Package version")]
+    [CommandOption("--version")]
+    public string? PackageVersion { get; set; }
+
+    [Description("Package version")]
+    [CommandOption("--manifest", IsHidden = true)]
+    public string? ManifestFile { get; set; }
+}
+
+public class UpdateWorkloadCommand : AsyncCommandWithProgress<UpdateWorkloadCommandSettings>
+{
+    public static readonly DirectoryInfo WorkloadDirectory = SecurityStorage.RootFolder.SubDirectory("workloads");
+    public static readonly Uri VEIN_GALLERY = new("https://api.vein-lang.org/");
+    public static readonly ShardStorage Storage = new();
+
+    public override async Task<int> ExecuteAsync(ProgressContext context, UpdateWorkloadCommandSettings settings)
+    {
+        var uninstallResult = await new UninstallWorkloadCommand().ExecuteAsync(context,
+            new UninstallWorkloadCommandSettings()
+            {
+                PackageName = settings.PackageName
+            });
+        if (uninstallResult != 0)
+            return uninstallResult;
+        var installResult = await new InstallWorkloadCommand().ExecuteAsync(context,
+            new InstallWorkloadCommandSettings()
+            {
+                PackageName = settings.PackageName,
+                ManifestFile = settings.ManifestFile,
+                PackageVersion = settings.PackageVersion
+            });
+        return installResult;
+    }
+}
+
