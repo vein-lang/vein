dcbd2100ede6fe9091ab432beb86c0157d2dee33
Yuuki Wesp <ls-micro@ya.ru>
2024-08-06 22:10:47 +0300

    ensure default methods in valueType and Object

--- runtime/common/reflection/VeinCore.cs
+++ runtime/common/reflection/VeinCore.cs
@@ -66,28 +66,109 @@ namespace vein.runtime
             var stdNamespace = NamespaceSymbol.Std;
 
             var coreModule = new VeinModule(stdModule, new Version(1, 0, 0), this);
-            ObjectClass = new(new (new ("Object"), stdNamespace, stdModule), (VeinClass)null, coreModule) { TypeCode = VeinTypeCode.TYPE_OBJECT };
-            ValueTypeClass = new(new(new("ValueType"), stdNamespace, stdModule), (VeinClass)null, coreModule) { TypeCode = VeinTypeCode.TYPE_OBJECT };
-            VoidClass = new(new(new("Void"), stdNamespace, stdModule), ValueTypeClass, coreModule) { TypeCode = VeinTypeCode.TYPE_VOID };
-            StringClass = new(new(new("String"), stdNamespace, stdModule), ObjectClass, coreModule) { TypeCode = VeinTypeCode.TYPE_STRING };
-            ByteClass = new(new(new("Byte"), stdNamespace, stdModule), ValueTypeClass, coreModule) { TypeCode = VeinTypeCode.TYPE_U1 };
-            SByteClass = new(new(new("SByte"), stdNamespace, stdModule), ValueTypeClass, coreModule) { TypeCode = VeinTypeCode.TYPE_I1 };
-            Int16Class = new(new(new("Int16"), stdNamespace, stdModule), ValueTypeClass, coreModule) { TypeCode = VeinTypeCode.TYPE_I2 };
-            Int32Class = new(new(new("Int32"), stdNamespace, stdModule), ValueTypeClass, coreModule) { TypeCode = VeinTypeCode.TYPE_I4 };
-            Int64Class = new(new(new("Int64"), stdNamespace, stdModule), ValueTypeClass, coreModule) { TypeCode = VeinTypeCode.TYPE_I8 };
-            UInt16Class = new(new(new("UInt16"), stdNamespace, stdModule), ValueTypeClass, coreModule) { TypeCode = VeinTypeCode.TYPE_U2 };
-            UInt32Class = new(new(new("UInt32"), stdNamespace, stdModule), ValueTypeClass, coreModule) { TypeCode = VeinTypeCode.TYPE_U4 };
-            UInt64Class = new(new(new("UInt64"), stdNamespace, stdModule), ValueTypeClass, coreModule) { TypeCode = VeinTypeCode.TYPE_U8 };
-            HalfClass = new(new(new("Half"), stdNamespace, stdModule), ValueTypeClass, coreModule) { TypeCode = VeinTypeCode.TYPE_R2 };
-            FloatClass = new(new(new("Float"), stdNamespace, stdModule), ValueTypeClass, coreModule) { TypeCode = VeinTypeCode.TYPE_R4 };
-            DoubleClass = new(new(new("Double"), stdNamespace, stdModule), ValueTypeClass, coreModule) { TypeCode = VeinTypeCode.TYPE_R8 };
-            DecimalClass = new(new(new("Decimal"), stdNamespace, stdModule), ValueTypeClass, coreModule) { TypeCode = VeinTypeCode.TYPE_R16 };
-            BoolClass = new(new(new("Boolean"), stdNamespace, stdModule), ValueTypeClass, coreModule) { TypeCode = VeinTypeCode.TYPE_BOOLEAN };
-            CharClass = new(new(new("Char"), stdNamespace, stdModule), ValueTypeClass, coreModule) { TypeCode = VeinTypeCode.TYPE_CHAR };
-            ExceptionClass = new(new(new("Exception"), stdNamespace, stdModule), ObjectClass, coreModule) { TypeCode = VeinTypeCode.TYPE_CLASS };
-            RawClass = new(new(new("Raw"), stdNamespace, stdModule), ObjectClass, coreModule) { TypeCode = VeinTypeCode.TYPE_RAW };
-            AspectClass = new(new(new("Aspect"), stdNamespace, stdModule), ObjectClass, coreModule) { TypeCode = VeinTypeCode.TYPE_CLASS };
-            FunctionClass = new(new(new("Function"), stdNamespace, stdModule), ObjectClass, coreModule) { TypeCode = VeinTypeCode.TYPE_FUNCTION };
+            ObjectClass = new(new (new ("Object"), stdNamespace, stdModule), (VeinClass)null, coreModule) {
+                TypeCode = VeinTypeCode.TYPE_OBJECT,
+                Flags = ClassFlags.NotCompleted | ClassFlags.Predefined
+            };
+            ValueTypeClass = new(new(new("ValueType"), stdNamespace, stdModule), (VeinClass)null, coreModule) {
+                TypeCode = VeinTypeCode.TYPE_OBJECT,
+                Flags = ClassFlags.NotCompleted | ClassFlags.Predefined
+            };
+            VoidClass = new(new(new("Void"), stdNamespace, stdModule), ValueTypeClass, coreModule) {
+                TypeCode = VeinTypeCode.TYPE_VOID,
+                Flags = ClassFlags.NotCompleted | ClassFlags.Predefined
+            };
+            StringClass = new(new(new("String"), stdNamespace, stdModule), ObjectClass, coreModule) {
+                TypeCode = VeinTypeCode.TYPE_STRING,
+                Flags = ClassFlags.NotCompleted | ClassFlags.Predefined
+            };
+            ByteClass = new(new(new("Byte"), stdNamespace, stdModule), ValueTypeClass, coreModule) {
+                TypeCode = VeinTypeCode.TYPE_U1,
+                Flags = ClassFlags.NotCompleted | ClassFlags.Predefined
+            };
+            SByteClass = new(new(new("SByte"), stdNamespace, stdModule), ValueTypeClass, coreModule) {
+                TypeCode = VeinTypeCode.TYPE_I1,
+                Flags = ClassFlags.NotCompleted | ClassFlags.Predefined
+            };
+            Int16Class = new(new(new("Int16"), stdNamespace, stdModule), ValueTypeClass, coreModule) {
+                TypeCode = VeinTypeCode.TYPE_I2,
+                Flags = ClassFlags.NotCompleted | ClassFlags.Predefined
+            };
+            Int32Class = new(new(new("Int32"), stdNamespace, stdModule), ValueTypeClass, coreModule) {
+                TypeCode = VeinTypeCode.TYPE_I4,
+                Flags = ClassFlags.NotCompleted | ClassFlags.Predefined
+            };
+            Int64Class = new(new(new("Int64"), stdNamespace, stdModule), ValueTypeClass, coreModule) {
+                TypeCode = VeinTypeCode.TYPE_I8,
+                Flags = ClassFlags.NotCompleted | ClassFlags.Predefined
+            };
+            UInt16Class = new(new(new("UInt16"), stdNamespace, stdModule), ValueTypeClass, coreModule) {
+                TypeCode = VeinTypeCode.TYPE_U2,
+                Flags = ClassFlags.NotCompleted | ClassFlags.Predefined
+            };
+            UInt32Class = new(new(new("UInt32"), stdNamespace, stdModule), ValueTypeClass, coreModule) {
+                TypeCode = VeinTypeCode.TYPE_U4,
+                Flags = ClassFlags.NotCompleted | ClassFlags.Predefined
+            };
+            UInt64Class = new(new(new("UInt64"), stdNamespace, stdModule), ValueTypeClass, coreModule) {
+                TypeCode = VeinTypeCode.TYPE_U8,
+                Flags = ClassFlags.NotCompleted | ClassFlags.Predefined
+            };
+            HalfClass = new(new(new("Half"), stdNamespace, stdModule), ValueTypeClass, coreModule) {
+                TypeCode = VeinTypeCode.TYPE_R2,
+                Flags = ClassFlags.NotCompleted | ClassFlags.Predefined
+            };
+            FloatClass = new(new(new("Float"), stdNamespace, stdModule), ValueTypeClass, coreModule) {
+                TypeCode = VeinTypeCode.TYPE_R4,
+                Flags = ClassFlags.NotCompleted | ClassFlags.Predefined
+            };
+            DoubleClass = new(new(new("Double"), stdNamespace, stdModule), ValueTypeClass, coreModule) {
+                TypeCode = VeinTypeCode.TYPE_R8,
+                Flags = ClassFlags.NotCompleted | ClassFlags.Predefined
+            };
+            DecimalClass = new(new(new("Decimal"), stdNamespace, stdModule), ValueTypeClass, coreModule) {
+                TypeCode = VeinTypeCode.TYPE_R16,
+                Flags = ClassFlags.NotCompleted | ClassFlags.Predefined
+            };
+            BoolClass = new(new(new("Boolean"), stdNamespace, stdModule), ValueTypeClass, coreModule) {
+                TypeCode = VeinTypeCode.TYPE_BOOLEAN,
+                Flags = ClassFlags.NotCompleted | ClassFlags.Predefined
+            };
+            CharClass = new(new(new("Char"), stdNamespace, stdModule), ValueTypeClass, coreModule) {
+                TypeCode = VeinTypeCode.TYPE_CHAR,
+                Flags = ClassFlags.NotCompleted | ClassFlags.Predefined
+            };
+            ExceptionClass = new(new(new("Exception"), stdNamespace, stdModule), ObjectClass, coreModule) {
+                TypeCode = VeinTypeCode.TYPE_CLASS,
+                Flags = ClassFlags.NotCompleted | ClassFlags.Predefined
+            };
+            RawClass = new(new(new("Raw"), stdNamespace, stdModule), ObjectClass, coreModule) {
+                TypeCode = VeinTypeCode.TYPE_RAW,
+                Flags = ClassFlags.NotCompleted | ClassFlags.Predefined
+            };
+            AspectClass = new(new(new("Aspect"), stdNamespace, stdModule), ObjectClass, coreModule) {
+                TypeCode = VeinTypeCode.TYPE_CLASS,
+                Flags = ClassFlags.NotCompleted | ClassFlags.Predefined
+            };
+            FunctionClass = new(new(new("Function"), stdNamespace, stdModule), ObjectClass, coreModule) {
+                TypeCode = VeinTypeCode.TYPE_FUNCTION,
+                Flags = ClassFlags.NotCompleted | ClassFlags.Predefined
+            };
+
+
+
+
+
+            ObjectClass.Methods =
+            [
+                new VeinMethod("toString", MethodFlags.Virtual | MethodFlags.Public, StringClass, ObjectClass, [],
+                    VeinArgumentRef.CreateThis(ObjectClass))
+            ];
+            ValueTypeClass.Methods =
+            [
+                new VeinMethod("toString", MethodFlags.Virtual | MethodFlags.Public, StringClass, ObjectClass, [],
+                    VeinArgumentRef.CreateThis(ObjectClass))
+            ];
         }
     }
 }
