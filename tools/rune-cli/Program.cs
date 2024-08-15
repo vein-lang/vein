e17e19cc06855045f6c9be4aab6be3bad9d5bbfd
Yuuki Wesp <ls-micro@ya.ru>
2024-08-15 11:07:46 +0300

    migrate to host builder, add toml configurator

--- tools/rune-cli/Program.cs
+++ tools/rune-cli/Program.cs
@@ -8,16 +8,23 @@ using System.Globalization;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
-using NuGet.Versioning;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using vein;
 using vein.cmd;
 using vein.json;
 using vein.project;
 using static Spectre.Console.AnsiConsole;
 using static vein.GlobalVersion;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Tomlyn.Extensions.Configuration;
+using vein.cli;
+using vein.services;
+
 [assembly: InternalsVisibleTo("veinc_test")]
 
-
+await AppMutex.Begin();
 JsonConvert.DefaultSettings = () => new JsonSerializerSettings
 {
     NullValueHandling = NullValueHandling.Ignore,
@@ -41,14 +48,9 @@ JsonConvert.DefaultSettings = () => new JsonSerializerSettings
 if (Environment.GetEnvironmentVariable("NO_CONSOLE") is not null)
     AnsiConsole.Console = RawConsole.Create();
 
-if (RuntimeInformation.IsOSPlatform(OSPlatform.FreeBSD))
-{
-    MarkupLine("Platform is not supported.");
-    return -1;
-}
-
-var skipIntro = SecurityStorage.HasKey("app:novid") ||
-                Environment.GetEnvironmentVariable("VEINC_NOVID") is not null;
+var skipIntro =
+    SecurityStorage.HasKey("app:novid") ||
+    Environment.GetEnvironmentVariable("VEINC_NOVID") is not null;
 
 var watch = Stopwatch.StartNew();
 
@@ -66,76 +68,87 @@ if (!skipIntro)
 
 AppFlags.RegisterArgs(ref args);
 
-var app = new CommandApp();
-
-app.Configure(config =>
-{
-    config.Settings.ApplicationVersion
+await Host.CreateDefaultBuilder(args)
+    .ConfigureLogging(x => x.SetMinimumLevel(LogLevel.None))
+    .ConfigureAppConfiguration(x => x.AddTomlFile(ShardStorage.VeinRootFolder.File("rune.toml").FullName, true))
+    .UseConsoleLifetime()
+    .UseSpectreConsole(config => {
+        config.Settings.ApplicationVersion
         = $"Vein Rune CLI {AssemblySemFileVer}\nBranch: {BranchName}+{ShortSha}\nCall rune workloads list for view installed workloads and other version";
-    config.AddCommand<NewCommand>("new")
-        .WithDescription("Create new project.");
-    config.AddCommand<CompileCommand>("build")
-        .WithDescription("Build current project.");
-    config.AddCommand<PackageCommand>("package")
-        .WithDescription("Prepare and build project to publish into package registry.");
-    config.AddCommand<CleanCommand>("clean")
-        .WithDescription("Clean project cache");
-    config.AddCommand<RestoreCommand>("restore")
-        .WithDescription("Restore dependencies in project.");
-    config.AddCommand<AddCommand>("add")
-        .WithDescription("Find and add package into project from registry")
-        .WithExample(["add std@0.12.1"]);
-    config.AddCommand<PublishCommand>("publish")
-        .WithDescription("Publish shard package into vein gallery. (need set 'packable: true' in project or call 'vein package')")
-        .WithExample(["--project ./foo.vproj"]);
-    config.AddBranch("workload", x =>
-    {
-        x.AddCommand<ListInstalledWorkloadCommand>("list")
-            .WithDescription($"Get list of installed workloads");
-        x.AddCommand<InstallWorkloadCommand>("install")
-            .WithDescription("Install workload into global");
-        x.AddCommand<InstallWorkloadCommand>("add")
-            .IsHidden()
-            .WithDescription("Install workload into global");
-        x.AddCommand<UpdateWorkloadCommand>("update")
-            .WithDescription("Update workload.");
-        x.AddCommand<UninstallWorkloadCommand>("uninstall")
-            .WithDescription("Uninstall workload.");
-        x.AddCommand<UninstallWorkloadCommand>("delete")
-            .IsHidden()
-            .WithDescription("Uninstall workload.");
-        x.AddCommand<UninstallWorkloadCommand>("remove")
-            .IsHidden()
-            .WithDescription("Uninstall workload.");
-    });
-    config.AddBranch("config", x =>
-    {
-        x.AddCommand<SetConfigCommand>("set")
-            .WithExample(["set foo:bar value"])
-            .WithExample(["set foo:zoo 'a sample value'"])
-            .WithDescription("Set value config by key in global storage.");
-        x.AddCommand<GetConfigCommand>("get")
-            .WithExample(["get foo:bar"])
-            .WithDescription("Get value config by key from global storage.");
-        x.AddCommand<ListConfigCommand>("list")
-            .WithDescription("Get all keys.");
-        x.AddCommand<RemoveConfigCommand>("remove")
-            .WithDescription("Remove key from global config.");
-    });
+        config.AddCommand<NewCommand>("new")
+            .WithDescription("Create new project.");
+        config.AddCommand<BuildCommand>("build")
+            .WithDescription("Build current project.");
+        config.AddCommand<PackageCommand>("package")
+            .WithDescription("Prepare and build project to publish into package registry.");
+        config.AddCommand<CleanCommand>("clean")
+            .WithDescription("Clean project cache");
+        config.AddCommand<RestoreCommand>("restore")
+            .WithDescription("Restore dependencies in project.");
+        config.AddCommand<AddCommand>("add")
+            .WithDescription("Find and add package into project from registry")
+            .WithExample(["add std@0.12.1"]);
+        config.AddCommand<PublishCommand>("publish")
+            .WithDescription("Publish shard package into vein gallery. (need set 'packable: true' in project or call 'vein package')")
+            .WithExample(["--project ./foo.vproj"]);
+        config.AddBranch("workload", x =>
+        {
+            x.AddCommand<ListInstalledWorkloadCommand>("list")
+                .WithDescription($"Get list of installed workloads");
+            x.AddCommand<InstallWorkloadCommand>("install")
+                .WithDescription("Install workload into global");
+            x.AddCommand<InstallWorkloadCommand>("add")
+                .IsHidden()
+                .WithDescription("Install workload into global");
+            x.AddCommand<UpdateWorkloadCommand>("update")
+                .WithDescription("Update workload.");
+            x.AddCommand<UninstallWorkloadCommand>("uninstall")
+                .WithDescription("Uninstall workload.");
+            x.AddCommand<UninstallWorkloadCommand>("delete")
+                .IsHidden()
+                .WithDescription("Uninstall workload.");
+            x.AddCommand<UninstallWorkloadCommand>("remove")
+                .IsHidden()
+                .WithDescription("Uninstall workload.");
+        });
+        config.AddBranch("config", x =>
+        {
+            x.AddCommand<SetConfigCommand>("set")
+                .WithExample(["set foo:bar value"])
+                .WithExample(["set foo:zoo 'a sample value'"])
+                .WithDescription("Set value config by key in global storage.");
+            x.AddCommand<GetConfigCommand>("get")
+                .WithExample(["get foo:bar"])
+                .WithDescription("Get value config by key from global storage.");
+            x.AddCommand<ListConfigCommand>("list")
+                .WithDescription("Get all keys.");
+            x.AddCommand<RemoveConfigCommand>("remove")
+                .WithDescription("Remove key from global config.");
+        });
 
-    config.SetExceptionHandler((ex) => {
+        config.SetExceptionHandler((ex) => {
 #if DEBUG
-        WriteException(ex);
+            WriteException(ex);
 #else
-        File.WriteAllText($"rune-error-{DateTimeOffset.Now:s}.txt", ex.ToString());
+            File.WriteAllText($"rune-error-{DateTimeOffset.Now:s}.txt", ex.ToString());
 #endif
-    });
-});
-
-var result = app.Run(args);
+        });
+    })
+    .ConfigureServices(
+        (ctx, services) =>
+        {
+            services.AddSingleton<ShardStorage>();
+            services.AddScoped((x) => new ShardRegistryQuery(
+                    new(ctx.Configuration.GetValue("galleryUrl", "https://api.vein-lang.org/")!))
+                .WithStorage(x.GetRequiredService<ShardStorage>()));
+            services.AddSingleton<WorkloadDb>();
+        })
+    .RunConsoleAsync();
 
 watch.Stop();
+await AppMutex.End();
 
 MarkupLine($":sparkles: Done in [lime]{watch.Elapsed.TotalSeconds:00.000}s[/].");
+return Environment.ExitCode;
 
-return result;
+
