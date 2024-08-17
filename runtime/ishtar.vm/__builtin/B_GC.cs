9609f4dd404711bc4004a838392a80bf2f9e11bc
Yuuki Wesp <ls-micro@ya.ru>
2024-08-17 09:30:24 +0300

    gc add AllocateBlock

--- runtime/ishtar.vm/__builtin/B_GC.cs
+++ runtime/ishtar.vm/__builtin/B_GC.cs
@@ -15,8 +15,28 @@ public static unsafe class B_GC
     public static IshtarObject* GetAliveObjects(CallFrame* current, IshtarObject** _)
         => current->GetGC()->ToIshtarObjectT(current->vm->gc->alive_objects, current);
 
+
+    private static IshtarObject* AllocateBlock(CallFrame* current, IshtarObject** args)
+    {
+        var span = new Vein_Span_u8(args[0]);
+        var bytes = current->vm->gc->AllocSpanTable(current, (uint)span._length);
+        span._ptr = bytes;
+        return null;
+    }
+
+    private static IshtarObject* DeAllocateBlock(CallFrame* current, IshtarObject** args)
+    {
+        var span = new Vein_Span_u8(args[0]);
+        current->vm->gc->FreeSpanTable(current, span._ptr);
+        return null;
+    }
+
     public static void InitTable(ForeignFunctionInterface ffi)
     {
+        ffi.Add("alloc_block([std]::std::Object) -> [std]::std::Void", ffi.AsNative(&AllocateBlock));
+        ffi.Add("dealloc_block([std]::std::Raw) -> [std]::std::Void", ffi.AsNative(&DeAllocateBlock));
+
+
         ffi.Add("i_call_GC_get_allocated", Public | Static | Extern, TYPE_I8)->
             AsNative((delegate*<CallFrame*, IshtarObject**, IshtarObject*>)&GetAllocatedBytes);
 
