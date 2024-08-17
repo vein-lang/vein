968361d122a5570856e5284fd2bca67e64b33428
Yuuki Wesp <ls-micro@ya.ru>
2024-08-17 09:33:49 +0300

    add profiler

---
+++ runtime/ishtar.vm/runtime/Profiler.cs
@@ -0,0 +1,30 @@
+namespace ishtar;
+
+public readonly struct Profiler : IDisposable
+{
+#if PROFILER
+    private readonly string _tag;
+    private readonly Stopwatch _watcher;
+#endif
+
+    private Profiler(string tag)
+    {
+#if PROFILER
+        _tag = tag;
+        _watcher = Stopwatch.StartNew();
+#endif
+    }
+
+    public static Profiler Begin(string tag)
+        => new Profiler(tag);
+
+    public void Complete()
+    {
+#if PROFILER
+        _watcher.Stop();
+        Console.WriteLine($"[{_tag}] elapsed {_watcher.ElapsedMilliseconds} ms");
+#endif
+    }
+
+    public void Dispose() => Complete();
+}
