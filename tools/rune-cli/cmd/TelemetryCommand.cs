a937b2ade220b3bb47089f455a6eed4b875d381e
Yuuki Wesp <ls-micro@ya.ru>
2024-08-18 15:44:12 +0300

    fix telemetry notice

--- tools/rune-cli/cmd/TelemetryCommand.cs
+++ tools/rune-cli/cmd/TelemetryCommand.cs
@@ -7,10 +7,13 @@ public class TelemetryCommand : Command
 {
     public override int Execute(CommandContext context)
     {
-        AnsiConsole.Write(new Rule($"[blue bold]Vein Telemetry[/]") { Style = Style.Parse("lime rapidblink") });
-        AnsiConsole.MarkupLine($"The [blue bold]VeinSDK[/] tools collect usage data in order to help us improve your experience.");
-        AnsiConsole.MarkupLine($"You [red bold]cannot disable[/] telemetry before the [blue bold]1.0[/] release.");
-        AnsiConsole.Write(new Rule { Style = Style.Parse("lime rapidblink") });
+        AnsiConsole.Write(new Rule($"[red bold]Telemetry Notice[/]") { Style = Style.Parse("orange rapidblink") });
+        AnsiConsole.MarkupLine($"");
+        AnsiConsole.MarkupLine($"\tThe [blue bold]VeinSDK[/] tools collect usage data in order to help us improve your experience.");
+        AnsiConsole.MarkupLine($"\tYou [red bold]cannot disable[/] telemetry before the [blue bold]1.0[/] release.");
+        AnsiConsole.MarkupLine($"\tIf you [red bold]do not agree[/] with this notification, [red bold]stop[/] the installation and [red bold]delete[/] the [gray bold]~/.vein[/] folder");
+        AnsiConsole.MarkupLine($"");
+        AnsiConsole.Write(new Rule { Style = Style.Parse("orange rapidblink") });
         return 0;
     }
 }
