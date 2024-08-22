d9fe79108c3dceba83125f5d85ca7e636f1541fd
Yuuki Wesp <ls-micro@ya.ru>
2024-08-22 16:59:22 +0300

    add Escapes

--- runtime/common/MarkupExtensions.cs
+++ runtime/common/MarkupExtensions.cs
@@ -4,5 +4,8 @@ namespace ishtar
     {
         public static string EscapeArgumentSymbols(this string str)
             => str.Replace("{", "{{").Replace("}", "}}");
+
+        public static string Escapes(this string str, char c)
+            => $"{c}{str}{c}";
     }
 }
