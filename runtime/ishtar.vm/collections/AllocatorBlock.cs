eae0899103ca618a71ee65789ae1b2c4c807c241
Yuuki Wesp <ls-micro@ya.ru>
2024-09-05 05:57:31 +0300

    drop unused code

--- runtime/ishtar.vm/collections/AllocatorBlock.cs
+++ runtime/ishtar.vm/collections/AllocatorBlock.cs
@@ -12,17 +12,7 @@ public readonly unsafe struct AllocatorBlock(void* parent,
 
     public readonly delegate*<uint, void*, void*> alloc_with_history = allocWithHistory;
     public readonly delegate*<uint, void*, void*> alloc_primitives_with_history = allocPrimitivesWithHistory;
-
-
+    
     public void* alloc(uint size) => alloc_with_history(size, parent);
     public void* alloc_primitives(uint size) => alloc_primitives_with_history(size, parent);
-}
-
-
-public static unsafe class AllocatorBlockEx
-{
-    public static T* malloc<T>(this AllocatorBlock allocator, uint size) where T : unmanaged
-        => (T*)allocator.alloc(size);
-    public static T* realloc<T>(this AllocatorBlock allocator, T* data, uint size) where T : unmanaged
-        => (T*)allocator.realloc(data, size);
 }
